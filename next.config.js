df85a90866658310e97b0fac7111e0396e825f8d
jeremyzilar <jeremyzilar@gmail.com>
2024-01-05 10:11:23 -0500

    add next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+}
 
 module.exports = nextConfig
+
+module.exports = {
+  productionBrowserSourceMaps: true,
+  trailingSlash: true,
+  images: {
+    domains: [
+      "localhost",
+    ],
+  },
+  
+}
