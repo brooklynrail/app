587d52d32051d07d921906c2efe0b153e59ecd0d
jeremyzilar <jeremyzilar@gmail.com>
2024-02-08 15:53:33 -0500

    change to tsx

---
+++ lib/directus.tsx
@@ -0,0 +1,7 @@
+import { createDirectus, rest } from "@directus/sdk"
+import { CustomDirectusTypes } from "./types"
+
+const path = process.env.BASE_DIRECTUS_URL ? process.env.BASE_DIRECTUS_URL : "http://localhost:8055"
+const directus = createDirectus(path).with(rest())
+
+export default directus
