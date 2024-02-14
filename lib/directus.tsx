032cc74311ca5794029ce11f2d2799e79fb18855
jeremyzilar <jeremyzilar@gmail.com>
2024-02-14 11:45:03 -0500

    add CustomDirectusTypes

--- lib/directus.tsx
+++ lib/directus.tsx
@@ -2,6 +2,6 @@ import { createDirectus, rest } from "@directus/sdk"
 import { CustomDirectusTypes } from "./types"
 
 const path = process.env.BASE_DIRECTUS_URL ? process.env.BASE_DIRECTUS_URL : "http://localhost:8055"
-const directus = createDirectus(path).with(rest())
+const directus = createDirectus<CustomDirectusTypes>(path).with(rest())
 
 export default directus
