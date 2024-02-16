7a82dff1cb6e74dd2cb7e0024307e467fefd21f3
jeremyzilar <jeremyzilar@gmail.com>
2024-02-16 11:07:22 -0500

    add RailPRojects

---
+++ src/components/homepage/railProjects.tsx
@@ -0,0 +1,27 @@
+const RailProjects = () => {
+  return (
+    <div className="related rail_projects">
+      <a href="https://shop.brooklynrail.org/collections/books" target="out-rail">
+        <img src="/images/banners-left/Rail-Ed-WEB.jpg" />
+      </a>
+
+      <a href="http://curatorialprojects.brooklynrail.org/occupy-mana" target="out-rail">
+        <img src="/images/banners-left/rail_curatorial_projects_logo.png" />
+      </a>
+
+      <a href="https://brooklynrail.org/special/RIVER_RAIL/">
+        <img src="/images/banners-left/river-rail-logo.png" />
+      </a>
+
+      <a href="http://miamirail.org/" target="out-rail">
+        <img src="/images/banners-left/miami_rail_logo.png" />
+      </a>
+
+      <a href="http://thirdrailquarterly.org/" target="out-rail">
+        <img src="/images/banners-left/third_rail_logo.jpg" />
+      </a>
+    </div>
+  )
+}
+
+export default RailProjects
