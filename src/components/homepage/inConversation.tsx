7e628fdeb3c1efd74d2185fe92a184530dbf6fb8
jeremyzilar <jeremyzilar@gmail.com>
2024-02-16 09:21:03 -0500

    use PromoStandard

--- src/components/homepage/inConversation.tsx
+++ src/components/homepage/inConversation.tsx
@@ -1,37 +1,4 @@
-import { CoverImage } from "../issueRail"
-import CoversPopup from "../issueRail/coversPopup"
-import Footer from "../footer"
-
-const PromoStandard = (props: any) => {
-  const { title, excerpt, dateSlug, slug, sections } = props
-  const sectionSlug = "art"
-  const section = "Art"
-  const kicker = "In Conversation"
-  const permalink = `/${dateSlug}/${sectionSlug}/${slug}`
-
-  return (
-    <>
-      <div className="promo promo-standard" itemType="http://schema.org/Article">
-        <p className="article_type">
-          <a className="section" href={`/${dateSlug}/${sectionSlug}`} title={`Go to the ${section} section`}>
-            {section}
-          </a>
-          <span className="divider"></span>
-          <span className="type">{kicker}</span>
-        </p>
-        <div className="media">
-          <a href={permalink}>
-            <img src="https://placehold.co/316x96/C57AFF/9D20FF" alt="" />
-          </a>
-        </div>
-        <h4>
-          <a href={permalink}>{title}</a>
-        </h4>
-        <p className="excerpt">{excerpt}</p>
-      </div>
-    </>
-  )
-}
+import PromoStandard from "../promo/standard"
 
 const InConversation = (props: any) => {
   const { inConversation, currentIssue } = props
@@ -41,7 +8,7 @@ const InConversation = (props: any) => {
   return (
     <section className="collection">
       {inConversation.map((article: any, i: number) => {
-        return <PromoStandard key={i} {...article} dateSlug={dateSlug} />
+        return <PromoStandard key={i} article={article} dateSlug={dateSlug} />
       })}
     </section>
   )
