dfd155f2340fd6da0d6f15b696636ff9b203b732
jeremyzilar <jeremyzilar@gmail.com>
2024-02-16 11:38:29 -0500

    add placeholder image for ad970

--- src/components/homepage/ad970.tsx
+++ src/components/homepage/ad970.tsx
@@ -7,6 +7,10 @@ interface AdsTileProps {
 
 const Ad970 = (props: AdsTileProps) => {
   const { ads } = props
+  const src = `https://placehold.co/1008x101/C57AFF/9D20FF`
+  const src_mobile = `https://placehold.co/1008x101/C57AFF/9D20FF`
+  const alt = `alt text for ad970`
+  const href = `https://example.com`
 
   return (
     <>
@@ -14,20 +18,9 @@ const Ad970 = (props: AdsTileProps) => {
         <p>Advertisement</p>
         <div>
           <div className="ad">
-            <a
-              href=" https://goldenartistcolors.com/products/qor-artist-watercolors-ali-cavanaugh-portrait-colors-set"
-              target="_blank"
-            >
-              <img
-                className="image_desktop"
-                alt="Advertisement: Golden Paint"
-                src="https://v5.airtableusercontent.com/v3/u/25/25/1707890400000/WPRHHsf09kftsowSoocwdw/bFfgTXwUcSXU_KF-bns9k9cC17wLV7bUTLxqzLPdIS5s_kzE-MwVfRDQlw1Fi4ebAoH3TriX1fucMASdR71tIged4YryIsSoSTwmZR6mN09gotzN8nAfbDqtVeZu-6yvkKs00pBg9R2-5WORpyM9gsL_UklFQ2WgFsFl3K_jutk/ze9x4wGsY8b1gtwCVVg3JHuwUpL9RRWZE5Qma1wRFUs"
-              />
-              <img
-                className="image_mobile"
-                alt="Advertisement: Golden Paint"
-                src="https://v5.airtableusercontent.com/v3/u/25/25/1707890400000/wikViUKRbFNARAlP535NxQ/Y3gZJWISdLBtIXs_yE-4JR1ZE7UCHEuxu8MUCDw4wbUm-pUU2zkHGB5GvR--qcwFqfVW8s6HQISMEf0Xd_4uSTZ9Zc49EwZ9jE7eu18NAXKQz3kVBzIPq99i3bKVs3FdY9ssReTnZ05-nZ7pPLyDNDdiVGrOqJXyU1WUlTcKpLM/amEK1rNRESuTmjSEMZxMTQkU7vkpgl84WP5G9bBVbzQ"
-              />
+            <a href={href} target="_blank">
+              <img className="image_desktop" alt={alt} src={src} />
+              <img className="image_mobile" alt={alt} src={src_mobile} />
             </a>
           </div>
         </div>
