7c267764333bf921ad20a8fb3cdf24e417783605
jeremyzilar <jeremyzilar@gmail.com>
2024-02-16 10:56:44 -0500

    create AdsTile component

---
+++ src/components/homepage/adsTile.tsx
@@ -0,0 +1,46 @@
+import { Ads } from "../../../lib/types"
+import Image from "next/image"
+
+interface AdsTileProps {
+  ads: Array<Ads>
+}
+
+const AdsTile = (props: AdsTileProps) => {
+  const { ads } = props
+
+  // limit ads to any 5 from the array
+  const limitedAds = ads.slice(0, 5)
+
+  return (
+    <>
+      <div id="extra">
+        <div id="ads">
+          <div className="ad_label">ADVERTISEMENTS</div>
+          <div className="ads_container">
+            {limitedAds.map((ad: Ads, i: number) => {
+              if (!ad.image || !ad.ad_url) {
+                return <></>
+              }
+              const width = ad.image.width ?? 0
+              const height = ad.image.height ?? 0
+              const src = `http://localhost:8055/assets/${ad.image.filename_disk}`
+              const size = 147
+              const scaledWidth = width > size ? size : width
+              const scaledHeight = width > size ? (size * height) / width : (height * scaledWidth) / width
+
+              return (
+                <div key={i} className="ad">
+                  <a href={ad.ad_url} target="_blank">
+                    <Image src={src} width={scaledWidth} height={scaledHeight} alt={`description`} />
+                  </a>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default AdsTile
--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -1,7 +1,6 @@
 import { CoverImage } from "../issueRail"
 import CoversPopup from "../issueRail/coversPopup"
 import Footer from "../footer"
-import Image from "next/image"
 import InConversation from "./inConversation"
 import { useState } from "react"
 import { Ads, Issues } from "../../../lib/types"
@@ -10,41 +9,8 @@ import { HomepageProps } from "@/pages"
 import CriticsPage from "./criticsPage"
 import EditorsMessage from "./editorsMessage"
 import PublishersMessage from "./publishersMessage"
+import AdsTile from "./adsTile"
 
-interface AdsTileProps {
-  ads: Array<Ads>
-}
-
-const AdsTile = (props: AdsTileProps) => {
-  const { ads } = props
-
-  // limit ads to any 5 from the array
-  const limitedAds = ads.slice(0, 5)
-
-  return (
-    <>
-      {limitedAds.map((ad: Ads, i: number) => {
-        if (!ad.image || !ad.ad_url) {
-          return <></>
-        }
-        const width = ad.image.width ?? 0
-        const height = ad.image.height ?? 0
-        const src = `http://localhost:8055/assets/${ad.image.filename_disk}`
-        const size = 147
-        const scaledWidth = width > size ? size : width
-        const scaledHeight = width > size ? (size * height) / width : (height * scaledWidth) / width
-
-        return (
-          <div key={i} className="ad">
-            <a href={ad.ad_url} target="_blank">
-              <Image src={src} width={scaledWidth} height={scaledHeight} alt={`description`} />
-            </a>
-          </div>
-        )
-      })}
-    </>
-  )
-}
 interface IssueSelectProps {
   allIssues: Array<Issues>
 }
@@ -1490,14 +1456,7 @@ const Homepage = (props: HomepageProps) => {
                   </div>
                 </div>
                 <div className="ad_column grid-col-2">
-                  <div id="extra">
-                    <div id="ads">
-                      <div className="ad_label">ADVERTISEMENTS</div>
-                      <div className="ads_container">
-                        <AdsTile ads={ads} />
-                      </div>
-                    </div>
-                  </div>
+                  <AdsTile ads={ads} />
                 </div>
               </div>
             </div>
