6dfff7f4151734f3b533f24009ae13f068728fba
jeremyzilar <jeremyzilar@gmail.com>
2024-02-27 09:48:02 -0500

    add SectionArticles

---
+++ src/components/sectionPage/sectionArticles.tsx
@@ -0,0 +1,25 @@
+import { Articles } from "../../../lib/types"
+import { PromoProps } from "."
+import PromoSection from "../promo/section"
+
+const SectionArticles = (props: PromoProps) => {
+  const { dateSlug, currentArticles } = props
+
+  return (
+    <section className="collection">
+      {currentArticles.map((article: Articles, i: number) => {
+        return (
+          <PromoSection
+            key={`article-${i}`}
+            article={article}
+            dateSlug={dateSlug}
+            showImage={true}
+            showSection={true}
+          />
+        )
+      })}
+    </section>
+  )
+}
+
+export default SectionArticles
