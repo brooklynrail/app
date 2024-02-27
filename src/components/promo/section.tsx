dea61ae6f18f2d7a3a8f597453de153bdfc02582
jeremyzilar <jeremyzilar@gmail.com>
2024-02-27 09:47:23 -0500

    add section promo

---
+++ src/components/promo/section.tsx
@@ -0,0 +1,53 @@
+import parse from "html-react-parser"
+import { DirectusFiles } from "../../../lib/types"
+import { stripHtml } from "string-strip-html"
+import Image from "next/image"
+import { PromoProps, PromoSectionName } from "./standard"
+
+const PromoImage = (props: DirectusFiles) => {
+  const { filename_disk } = props
+  const src = `http://localhost:8055/assets/${filename_disk}?key=promo-section`
+  return <Image src={src} width={200} height={260} alt={"tktk"} />
+}
+
+const PromoSection = (props: PromoProps) => {
+  const { dateSlug, article, showSection, showImage } = props
+  const { title, excerpt, slug, sections, sort, featured_image } = article
+  const sectionSlug = sections[0].sections_id.slug
+  const permalink = `/${dateSlug}/${sectionSlug}/${slug}`
+
+  const sortNum = (
+    <span className="sort">
+      <span>{sort}</span>
+    </span>
+  )
+
+  return (
+    <>
+      <div className="promo promo-section" itemType="http://schema.org/Article">
+        <div className="grid-row grid-gap-4">
+          <div className="grid-col-12 tablet:grid-col-8">
+            {showSection && <PromoSectionName {...props} />}
+            <h4>
+              {sortNum}
+              <a href={permalink} title={`Visit ${stripHtml(title).result}`}>
+                {parse(title)}
+              </a>
+            </h4>
+            <p className="excerpt">{parse(excerpt)}</p>
+          </div>
+          <div className="grid-col-12 tablet:grid-col-4">
+            {showImage && featured_image && (
+              <div className={`media`}>
+                <a href={permalink} title={`Visit ${stripHtml(title).result}`}>
+                  <PromoImage {...featured_image} />
+                </a>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default PromoSection
