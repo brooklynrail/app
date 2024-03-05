8d3323882318bdb16f09d1df6fed5219581f76eb
jeremyzilar <jeremyzilar@gmail.com>
2024-03-05 14:20:41 -0500

    optimize query to return less data

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -25,7 +25,32 @@ export async function getIssuesSelect() {
 export async function getIssueData(year?: number, month?: number) {
   const issueData = await directus.request(
     readItems("issues", {
-      fields: ["*.*"],
+      fields: [
+        "id",
+        "title",
+        "slug",
+        "year",
+        "month",
+        "status",
+        {
+          cover_1: ["caption", "filename_disk", "width", "height"],
+        },
+        {
+          cover_2: ["caption", "filename_disk", "width", "height"],
+        },
+        {
+          cover_3: ["caption", "filename_disk", "width", "height"],
+        },
+        {
+          cover_4: ["caption", "filename_disk", "width", "height"],
+        },
+        {
+          cover_5: ["caption", "filename_disk", "width", "height"],
+        },
+        {
+          cover_6: ["caption", "filename_disk", "width", "height"],
+        },
+      ],
       filter: {
         _and: [
           { year: year && month ? { _eq: year } : undefined },
@@ -41,7 +66,18 @@ export async function getIssueData(year?: number, month?: number) {
 export function getSectionsByIssueId(issueId: number) {
   const sections = directus.request(
     readItems("sections", {
-      fields: ["name", "slug", "articles.articles_slug.issues.issues_id.id"],
+      // fields: ["name", "slug", "articles", "articles.articles_slug.issues.issues_id.id"],
+      fields: [
+        "name",
+        "slug",
+        {
+          articles: [
+            {
+              articles_slug: ["issues.issues_id.id"],
+            },
+          ],
+        },
+      ],
       filter: {
         _and: [
           {
@@ -66,17 +102,34 @@ export function getArticles(issueId: number, section?: string) {
         "slug",
         "excerpt",
         "kicker",
-        "promo_thumb.*",
-        "promo_banner.*",
-        "slideshow_image.*",
-        "featured_image.*",
+        {
+          promo_thumb: ["caption", "filename_disk", "width", "height"],
+        },
+        {
+          promo_banner: ["caption", "filename_disk", "width", "height"],
+        },
+        {
+          slideshow_image: ["caption", "filename_disk", "width", "height"],
+        },
+        {
+          featured_image: ["caption", "filename_disk", "width", "height"],
+        },
         "featured",
         "sort",
-        "contributors.contributors_id.first_name",
-        "contributors.contributors_id.last_name",
-        "contributors.contributors_id.slug",
-        "sections.sections_id.slug",
-        "sections.sections_id.name",
+        {
+          contributors: [
+            {
+              contributors_id: ["first_name", "last_name", "slug"],
+            },
+          ],
+        },
+        {
+          sections: [
+            {
+              sections_id: ["slug", "name"],
+            },
+          ],
+        },
       ],
       filter: {
         _and: [
@@ -94,11 +147,42 @@ export function getArticles(issueId: number, section?: string) {
 }
 
 export function getAds() {
+  // get todays date and format it like year-mo-day with leading zeros
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : `${today.getMonth() + 1}`
+  const day = today.getDate() < 10 ? `0${today.getDate()}` : `${today.getDate()}`
+  const formattedDate = `${year}-${month}-${day}`
+
   const ads = directus.request(
     readItems("ads", {
-      fields: ["*.*"],
+      fields: [
+        "ad_type",
+        "ad_url",
+        "start_date",
+        "end_date",
+        "status",
+        "sort",
+        "title",
+        {
+          tile_image: ["filename_disk", "width", "height"],
+        },
+        {
+          banner_image: ["filename_disk", "width", "height"],
+        },
+        {
+          banner_image_mobile: ["filename_disk", "width", "height"],
+        },
+      ],
       filter: {
-        _and: [{ status: { _eq: "published" }, start_date: { _gte: "2021-01-01" }, ad_url: { _nnull: true } }],
+        _and: [
+          {
+            status: { _eq: "published" },
+            start_date: { _lte: formattedDate },
+            end_date: { _gte: formattedDate },
+            ad_url: { _nnull: true },
+          },
+        ],
       },
     }),
   )
