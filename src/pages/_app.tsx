ff782dd0d45a480baac9fc790ebff3c0600273a7
jeremyzilar <jeremyzilar@gmail.com>
2024-03-06 14:40:22 -0500

    add DefaultSeo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,26 @@
 import type { AppProps } from "next/app"
 import "../../styles/uswds/styles.scss"
 import { PopupProvider } from "@/components/issueRail/popupProvider"
+import { DefaultSeo } from "next-seo"
 
 function RailApp({ Component, pageProps }: AppProps) {
   return (
     <PopupProvider>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          description:
+            "The Brooklyn Rail is a journal committed to providing an independent forum for visual arts, culture, and politics throughout New York City and beyond.",
+          url: "https://brooklynrail.org/",
+          siteName: "The Brooklyn Rail",
+        }}
+        twitter={{
+          handle: "@thebrooklynrail",
+          site: "@brooklynrail",
+          cardType: "summary_large_image",
+        }}
+      />
       <Component {...pageProps} />
     </PopupProvider>
   )
