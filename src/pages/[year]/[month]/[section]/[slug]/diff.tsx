51c99951cd89d7bc76e2dfd7613595e307e7e29b
jeremyzilar <jeremyzilar@gmail.com>
2024-03-28 12:03:49 -0400

    check for currentSection

--- src/pages/[year]/[month]/[section]/[slug]/diff.tsx
+++ src/pages/[year]/[month]/[section]/[slug]/diff.tsx
@@ -32,9 +32,9 @@ export async function getStaticProps({ params }: any) {
   const article = articleData
   const currentIssue = issueData[0]
   const sections = sectionsData
-  const currentSection: Sections = article.sections && article.sections[0].sections_id
+  const currentSection = article.sections && article.sections[0].sections_id
 
-  const errorCode = currentSection.slug != section && "Section not found"
+  const errorCode = !currentSection || (currentSection.slug != section && "Section not found")
   if (errorCode) {
     return { props: { errorCode: 404, errorMessage: errorCode } }
   }
