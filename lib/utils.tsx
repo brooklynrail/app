7ba401b33c93f97d097aad4c840a4e4227a9847e
jeremyzilar <jeremyzilar@gmail.com>
2024-03-28 10:27:47 -0400

    Revert "fix field filters"
    This reverts commit 6110e65af29a9f0a6dccb204343330eb58818f6e.

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -93,7 +93,7 @@ export async function getIssueData(year?: number, month?: number) {
                   contributors: [{ contributors_id: ["first_name", "last_name", "slug"] }],
                 },
                 {
-                  sections: [{ sections_id: ["slug"] }],
+                  sections: ["sections_id.slug"],
                 },
                 "sort",
               ],
@@ -123,7 +123,7 @@ export function getSectionsByIssueId(issueId: number) {
         {
           articles: [
             {
-              articles_slug: [{ issues: [{ issues_id: ["slug"] }] }],
+              articles_slug: ["issues.issues_id.id"],
             },
           ],
         },
