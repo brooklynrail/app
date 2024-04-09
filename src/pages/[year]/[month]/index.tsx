d7084aba4b4eecf2a54847a399424654914b1917
jeremyzilar <jeremyzilar@gmail.com>
2024-04-09 14:27:31 -0400

    rm log

--- src/pages/[year]/[month]/index.tsx
+++ src/pages/[year]/[month]/index.tsx
@@ -100,7 +100,6 @@ export async function getStaticPaths() {
       fields: ["year", "month"],
     }),
   )
-  console.log(issues)
 
   const paths = issues.map((issue) => {
     const month = issue.month
