b2b5fdea55e687ad1974b35911a68542b09e6cb8
jeremyzilar <jeremyzilar@gmail.com>
2024-04-10 13:00:37 -0400

    add railIssue API

---
+++ src/pages/api/railIssue.tsx
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { getRailIssueApi } from "../../../lib/utils"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const year = Number(req.query.year)
+  const month = Number(req.query.month)
+
+  const data = await getRailIssueApi(year, month)
+
+  // Set Cache-Control header
+  // This will cache the response for 1 hour
+  res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=60")
+
+  res.status(200).json(data)
+}
