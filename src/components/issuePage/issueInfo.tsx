e90709cdcbf94df32ec2988b193c83deb0685186
jeremyzilar <jeremyzilar@gmail.com>
2024-04-10 13:00:28 -0400

    add issue_info page

---
+++ src/components/issuePage/issueInfo.tsx
@@ -0,0 +1,134 @@
+import { DirectusFiles } from "../../../lib/types"
+import { IssuePageProps } from "@/pages"
+import { IssueInfoProps } from "@/pages/[year]/[month]/info"
+import Image from "next/image"
+
+interface IssueCoversProps {
+  cover_1: DirectusFiles | undefined
+  cover_2: DirectusFiles | undefined
+  cover_3: DirectusFiles | undefined
+  cover_4: DirectusFiles | undefined
+  cover_5: DirectusFiles | undefined
+  cover_6: DirectusFiles | undefined
+}
+
+const IssueCovers = (props: IssueCoversProps) => {
+  const { cover_1, cover_2, cover_3, cover_4, cover_5, cover_6 } = props
+  const covers = [cover_1, cover_2, cover_3, cover_4, cover_5, cover_6]
+  // for each props, if it exists, render it
+  const allCovers = covers.map((cover: any, i: number) => {
+    if (cover == null || cover == undefined) {
+      return
+    }
+
+    const width = (cover.width * 75) / cover.height
+    const height = (cover.height * width) / cover.width
+    const src = `${process.env.NEXT_PUBLIC_IMAGE_PATH}${cover.filename_disk}`
+    const alt = cover.description ? cover.description.replace(/(<([^>]+)>)/gi, "") : "The Brooklyn Rail"
+    const desc = cover.description ? (
+      <figcaption dangerouslySetInnerHTML={{ __html: `<strong>Cover ${i + 1}:</strong> ${cover.description}` }} />
+    ) : null
+    const image = (
+      <div key={`cover-${i}`} className="cover">
+        <Image src={src} width={width} height={height} alt={alt} />
+        {desc}
+      </div>
+    )
+    return image
+  })
+
+  return <div className="issue-covers">{allCovers}</div>
+}
+const IssueInfo = (props: IssueInfoProps) => {
+  const { currentIssue, permalink, currentSections, railIssueData } = props
+  const {
+    cover_1,
+    cover_2,
+    cover_3,
+    cover_4,
+    cover_5,
+    cover_6,
+    year,
+    month,
+    title,
+    slug,
+    special_issue,
+    issue_number,
+    old_id,
+    articles,
+  } = currentIssue
+  const coverImageProps = { cover_1, cover_2, cover_3, cover_4, cover_5, cover_6 }
+
+  console.log("currentIssue", currentIssue)
+  console.log("articles", articles)
+  // console.log("currentSections", currentSections)
+  // console.log("railIssueData", railIssueData)
+
+  const old = railIssueData
+
+  const row = function (label: string, data: any, oldData: any) {
+    // check if data and oldData are the same value
+    const diff = String(data) === String(oldData) ? "same" : "different"
+
+    return (
+      <tr className={diff}>
+        <th>{label}</th>
+        <td>{data}</td>
+        <td>{oldData}</td>
+      </tr>
+    )
+  }
+
+  return (
+    <>
+      <div className="paper">
+        <div className="hatbox"></div>
+        <div className="wrapper home">
+          <section id="main">
+            <div className="grid-container grid-container-desktop">
+              <div className="grid-row grid-gap-4">
+                <div className="grid-col-12">
+                  <div className="issue-info">
+                    <h1>{title}</h1>
+
+                    <table>
+                      <thead>
+                        <tr>
+                          <th></th>
+                          <th>Current site</th>
+                          <th>Old site</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {row("title", title, old.title)}
+                        {row("permalink", permalink, `https://brooklynrail.org/${old.slug}/`)}
+                        {row("slug", slug, old.slug)}
+                        {row("old_id", old_id, old.old_id)}
+                        {row("year", year, old.year)}
+                        {row("month", month, old.month)}
+                        {row("issue_number:", issue_number, old.issue_number)}
+                        {row("special issue:", special_issue ? "true" : "false", old.special_issue ? "true" : "false")}
+                        <tr>
+                          <th>covers:</th>
+                          <td>
+                            <IssueCovers {...coverImageProps} />
+                          </td>
+                          <td>
+                            <IssueCovers {...coverImageProps} />
+                          </td>
+                        </tr>
+                        {row("articles:", articles ? articles.length : "no articles", old.articles.length)}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default IssueInfo
--- src/pages/[year]/[month]/info.tsx
+++ src/pages/[year]/[month]/info.tsx
@@ -1,42 +1,33 @@
 import directus from "../../../../lib/directus"
 import { readItems } from "@directus/sdk"
-import IssuePage from "@/components/issuePage"
-import { IssuePageProps } from "@/pages"
 import {
   PageType,
-  getAds,
   getIssueData,
   getIssues,
-  getOGImage,
   getPermalink,
+  getRailIssueApi,
   getSectionsByIssueId,
 } from "../../../../lib/utils"
 import { NextSeo } from "next-seo"
-import { stripHtml } from "string-strip-html"
-import { ArticlesIssues } from "../../../../lib/types"
 
-function Issue(props: IssuePageProps) {
-  const { title, cover_1, issue_number, slug } = props.currentIssue
-  const ogtitle = `${stripHtml(title).result} | The Brooklyn Rail`
-  const ogdescription = `Issue #${issue_number} of The Brooklyn Rail`
-  const ogimageprops = { ogimage: cover_1, title }
-  const ogimages = getOGImage(ogimageprops)
+import IssueInfo from "@/components/issuePage/issueInfo"
+import { Issues, Sections } from "../../../../lib/types"
 
+export interface IssueInfoProps {
+  allIssues: Array<Issues>
+  currentIssue: Issues
+  currentSections: Array<Sections>
+  railIssueData: any
+  permalink: string
+  errorCode?: number
+  errorMessage?: string
+}
+
+function Issue(props: IssueInfoProps) {
   return (
     <>
-      <NextSeo
-        title={ogtitle}
-        description={ogdescription}
-        canonical={`${process.env.NEXT_PUBLIC_BASE_URL}/${slug}/`}
-        openGraph={{
-          title: ogtitle,
-          description: ogdescription,
-          url: `${process.env.NEXT_PUBLIC_BASE_URL}/${slug}/`,
-          images: ogimages,
-          type: `website`,
-        }}
-      />
-      <IssuePage {...props} />
+      <NextSeo />
+      <IssueInfo {...props} />
     </>
   )
 }
@@ -48,25 +39,13 @@ export async function getStaticProps({ params }: any) {
   const month = params.month
 
   const allIssues = await getIssues()
-  const issueData = await getIssueData(year, month)
+  const currentIssue = await getIssueData(year, month)
 
   // Get only the sections that are used in the articles in the current issue
-  const currentSections = await getSectionsByIssueId(issueData.id)
-
-  const currentArticles = issueData.articles
-
-  // Get the published Ads
-  const ads = await getAds()
-
-  // Filter the currentArticles to get only the articles with a slideshow image
-  const currentSlides: ArticlesIssues[] = []
-  currentArticles.forEach((articleIssue: ArticlesIssues) => {
-    if (articleIssue.articles_slug.slideshow_image) {
-      currentSlides.push(articleIssue)
-    }
-  })
-
-  const currentIssue = issueData
+  const currentSections = await getSectionsByIssueId(currentIssue.id)
+  // const railIssueData = await getRailIssueApi(year, month)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/railIssue?year=${year}&month=${month}`)
+  const railIssueData = await response.json()
 
   const permalink = getPermalink({
     year: currentIssue.year,
@@ -79,9 +58,7 @@ export async function getStaticProps({ params }: any) {
       allIssues,
       currentIssue,
       currentSections,
-      currentArticles,
-      currentSlides,
-      ads,
+      railIssueData,
       permalink,
     },
     // Next.js will attempt to re-generate the page:
