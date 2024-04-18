4f8c1160014a57292a9649bf9b06a35f95903bd5
jeremyzilar <jeremyzilar@gmail.com>
2024-04-17 23:23:54 -0400

    export PromoImage

--- src/components/promo/section.tsx
+++ src/components/promo/section.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 import { PromoProps, PromoSectionName } from "./standard"
 import Link from "next/link"
 
-const PromoImage = (props: DirectusFiles) => {
+export const PromoImage = (props: DirectusFiles) => {
   const { filename_disk } = props
   const src = `${process.env.NEXT_PUBLIC_IMAGE_PATH}${filename_disk}?key=promo-section`
   return <Image src={src} width={200} height={260} alt={"tktk"} />
