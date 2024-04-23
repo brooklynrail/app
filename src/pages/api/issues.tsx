7ea13da3b8c78064970fef00555f6fd7c231a1d0
jeremyzilar <jeremyzilar@gmail.com>
2024-04-23 17:23:57 -0600

    add additional fields

--- src/pages/api/issues.tsx
+++ src/pages/api/issues.tsx
@@ -4,20 +4,18 @@ import { readItems } from "@directus/sdk"
 
 // pages/api/issues.js
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { query } = req
-
-  if (!query) {
-    return res.status(400).json({ errorCode: 400, errorMessage: "This issue does not exist" })
-  }
-
   const issues = await directus.request(
     readItems("issues", {
-      fields: ["year", "month", "special_issue", "id"],
+      fields: ["year", "month", "id", "slug", "title", "special_issue", "issue_number"],
       filter: {
-        _and: [{ special_issue: { _eq: false } }],
+        _and: [{ special_issue: { _eq: false }, status: { _eq: "published" } }],
       },
     }),
   )
+
+  if (!issues) {
+    return res.status(400).json({ errorCode: 400, errorMessage: "Oddly, there are no issues getting returned." })
+  }
 
   // You can now use `year` and `month` to fetch data, perform calculations, or return a response
   res.status(200).json(issues)
