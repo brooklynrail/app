18a1981dd1a3d06bd5499c6db93c50ca0fc3a74f
jeremyzilar <jeremyzilar@gmail.com>
2024-05-02 14:52:36 -0400

    use getAllIssues

--- src/pages/api/issues.tsx
+++ src/pages/api/issues.tsx
@@ -1,22 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import directus from "../../../lib/directus"
-import { readItems } from "@directus/sdk"
+import { getAllIssues } from "../../../lib/utils"
 
 // pages/api/issues.js
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const issues = await directus.request(
-    readItems("issues", {
-      fields: ["year", "month", "id", "slug", "title", "special_issue", "issue_number"],
-      filter: {
-        _and: [{ special_issue: { _eq: false }, status: { _eq: "published" } }],
-      },
-    }),
-  )
+  const allIssues = await getAllIssues()
 
-  if (!issues) {
+  if (!allIssues) {
     return res.status(400).json({ errorCode: 400, errorMessage: "Oddly, there are no issues getting returned." })
   }
 
   // You can now use `year` and `month` to fetch data, perform calculations, or return a response
-  res.status(200).json(issues)
+  res.status(200).json(allIssues)
 }
