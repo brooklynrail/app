f3e0b06834b32fcd087baabd276228c0ca3d850f
jeremyzilar <jeremyzilar@gmail.com>
2024-05-04 17:53:40 -0400

    use NextImage

--- src/components/issuePage/railProjects.tsx
+++ src/components/issuePage/railProjects.tsx
@@ -1,26 +1,32 @@
 import Link from "next/link"
+import Image from "next/image"
 
 const RailProjects = () => {
   return (
     <div className="related rail_projects">
       <Link href="https://shop.brooklynrail.org/collections/books" target="out-rail">
-        <img src="/images/banners-left/Rail-Ed-WEB.jpg" />
+        <Image src="/images/banners-left/BR-Books-WEB.jpg" width={120} height={73} alt="Rail Editions" />
       </Link>
 
       <Link href="http://curatorialprojects.brooklynrail.org/occupy-mana" target="out-rail">
-        <img src="/images/banners-left/rail_curatorial_projects_logo.png" />
+        <Image
+          src="/images/banners-left/rail_curatorial_projects_logo.png"
+          width={120}
+          height={81}
+          alt="Rail Curatorial Projects"
+        />
       </Link>
 
       <Link href="https://brooklynrail.org/special/RIVER_RAIL/">
-        <img src="/images/banners-left/river-rail-logo.png" />
+        <Image src="/images/banners-left/river-rail-logo.png" width={120} height={20} alt="River Rail" />
       </Link>
 
       <Link href="http://miamirail.org/" target="out-rail">
-        <img src="/images/banners-left/miami_rail_logo.png" />
+        <Image src="/images/banners-left/miami_rail_logo.png" width={120} height={34} alt="Miami Rail" />
       </Link>
 
       <Link href="http://thirdrailquarterly.org/" target="out-rail">
-        <img src="/images/banners-left/third_rail_logo.jpg" />
+        <Image src="/images/banners-left/third_rail_logo.jpg" width={120} height={87} alt="Third Rail" />
       </Link>
     </div>
   )
