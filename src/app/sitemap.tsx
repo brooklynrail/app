a48f2920f9f770dc87a3b296e875c36e63abdee8
jeremyzilar <jeremyzilar@gmail.com>
2024-05-08 23:35:25 -0400

    add issues and special issues and articles

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,11 +1,10 @@
 import { MetadataRoute } from "next"
-import { getArticlePages } from "../../lib/utils"
-import { Articles } from "../../lib/types"
+import { getArticlePages, getIssues, getSpecialArticlePages } from "../../lib/utils"
+import { Articles, Issues } from "../../lib/types"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const articlePages = await getArticlePages()
-
-  const paths = articlePages.map((article: Articles) => {
+  const articles = articlePages.map((article: Articles) => {
     const year = article.issues[0].issues_id.year
     const month = article.issues[0].issues_id.month
     const section = article.sections[0].sections_id.slug
@@ -19,5 +18,42 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     }
   })
 
-  return [...paths]
+  const specialArticlePages = await getSpecialArticlePages()
+  const specialArticles = specialArticlePages.map((article: Articles) => {
+    const slug = article.slug
+    const issueSlug = article.issues && article.issues[0].issues_id.slug
+    const section = article.sections && article.sections[0].sections_id.slug
+
+    return {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/special/${issueSlug}/${section}/${slug}/`,
+      lastModified: article.date_updated,
+      changeFrequency: "weekly" as const,
+      priority: 1,
+    }
+  })
+
+  const allIssues = await getIssues({ special_issue: false })
+  const issues = allIssues.map((issue: Issues) => {
+    const year = issue.year
+    const month = issue.month < 10 ? String(`0${issue.month}`) : String(issue.month)
+    return {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${year}/${month}/`,
+      lastModified: issue.date_updated,
+      changeFrequency: "monthly" as const,
+      priority: 0.5,
+    }
+  })
+
+  const allSpecialIssues = await getIssues({ special_issue: true })
+  const specialIssues = allSpecialIssues.map((issue: Issues) => {
+    const issueSlug = issue.slug
+    return {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/special/${issueSlug}/`,
+      lastModified: issue.date_updated,
+      changeFrequency: "monthly" as const,
+      priority: 0.5,
+    }
+  })
+
+  return [...articles, ...specialArticles, ...issues, ...specialIssues]
 }
