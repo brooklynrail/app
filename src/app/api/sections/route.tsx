7d42990d95bb76dbc1062ab274db1a11503306ee
jeremyzilar <jeremyzilar@gmail.com>
2024-05-09 12:09:10 -0400

    update sections API to optionally get all the sections for an issue when issueId is passed in

--- src/app/api/sections/route.tsx
+++ src/app/api/sections/route.tsx
@@ -1,12 +1,34 @@
 import { readItems } from "@directus/sdk"
 import directus from "../../../../lib/directus"
 
-export async function GET() {
-  const data = await directus.request(
-    readItems("sections", {
-      fields: ["id", "name", "slug"],
-    }),
-  )
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const issueId = searchParams.get("issueId")
 
-  return Response.json(data)
+  if (issueId) {
+    const data = await directus.request(
+      readItems("sections", {
+        fields: ["id", "name", "slug"],
+        filter: {
+          _and: [
+            {
+              articles: {
+                articles_slug: {
+                  issues: { issues_id: { id: { _eq: issueId } } },
+                },
+              },
+            },
+          ],
+        },
+      }),
+    )
+    return Response.json(data)
+  } else {
+    const data = await directus.request(
+      readItems("sections", {
+        fields: ["id", "name", "slug"],
+      }),
+    )
+    return Response.json(data)
+  }
 }
