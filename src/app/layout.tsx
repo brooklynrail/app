f65e9f06e78ded29de6d27644a9a0c18ad71fbcf
jeremyzilar <jeremyzilar@gmail.com>
2024-05-09 14:12:58 -0400

    add Vercel Analytics and Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "../../styles/uswds/styles.scss"
 import { config } from "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 config.autoAddCss = false
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "The Brooklyn Rail",
@@ -11,8 +13,12 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+      <Analytics />
+      <SpeedInsights />
+    </>
   )
 }
