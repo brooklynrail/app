9b95b7499362a90275f81813ccbe569d7c00a5eb
jeremyzilar <jeremyzilar@gmail.com>
2024-05-08 21:57:38 -0400

    update sitemap

--- src/app/[year]/[month]/[section]/sitemap.tsx
+++ src/app/[year]/[month]/[section]/sitemap.tsx
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next"
-import { getArticlePages } from "../../../../../../lib/utils"
-import { Articles } from "../../../../../../lib/types"
+import { getArticlePages } from "../../../../../lib/utils"
+import { Articles } from "../../../../../lib/types"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const articlePages = await getArticlePages()
@@ -9,10 +9,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const year = article.issues[0].issues_id.year
     const month = article.issues[0].issues_id.month
     const section = article.sections[0].sections_id.slug
-    const slug = article.slug
 
     return {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${year}/${month}/${section}/${slug}/`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${year}/${month}/${section}/`,
       lastModified: article.date_updated,
       changeFrequency: "weekly" as const,
       priority: 1,
