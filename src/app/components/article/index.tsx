9d1b4216824b4073be00c061e3a5d3e9a164e835
jeremyzilar <jeremyzilar@gmail.com>
2024-05-10 16:11:26 -0400

    remove featured-image transform

--- src/app/components/article/index.tsx
+++ src/app/components/article/index.tsx
@@ -23,8 +23,8 @@ interface FeaturedImageProps {
   title: string
 }
 const FeaturedImage = (props: FeaturedImageProps) => {
-  const { filename_disk, caption } = props.image
-  const src = `${process.env.NEXT_PUBLIC_IMAGE_PATH}${filename_disk}?key=featured-image`
+  const { filename_disk, caption, width, height } = props.image
+  const src = `${process.env.NEXT_PUBLIC_IMAGE_PATH}${filename_disk}`
   const desc = caption ? <figcaption>{parse(caption)}</figcaption> : null
   const alt = caption ? caption : `${stripHtml(props.title).result}`
 
@@ -35,8 +35,8 @@ const FeaturedImage = (props: FeaturedImageProps) => {
           priority
           src={src}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          width={400}
-          height={529}
+          width={width}
+          height={height}
           style={{
             width: "100%",
             height: "auto",
