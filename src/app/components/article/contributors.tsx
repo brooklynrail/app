26f9ea9be28e0487fde26ad3777e44ca936ee711
jeremyzilar <jeremyzilar@gmail.com>
2024-05-09 22:45:53 -0400

    Fix the contributors box at the bottom of articles when there is more than one contributor

--- src/app/components/article/contributors.tsx
+++ src/app/components/article/contributors.tsx
@@ -7,23 +7,28 @@ interface ContributorsProps {
 
 const ContributorsBox = (props: ContributorsProps) => {
   const { contributors } = props
+
+  const title = contributors.length > 1 ? "Contributors" : "Contributor"
+  const authors = contributors.map((contributor: any, i: number) => {
+    return (
+      <div key={i}>
+        <h4>
+          <Link href={`/contributor/${contributor.contributors_id.slug}`}>
+            {contributor.contributors_id.first_name} {contributor.contributors_id.last_name}
+          </Link>
+        </h4>
+        <p dangerouslySetInnerHTML={{ __html: contributor.contributors_id.bio }} />
+      </div>
+    )
+  })
   return (
     <cite>
-      {contributors.map((contributor: any, i: number) => {
-        return (
-          <footer key={i}>
-            <section className="contributors">
-              <h3>Contributor</h3>
-              <h4>
-                <Link href={`/contributor/${contributor.contributors_id.slug}`}>
-                  {contributor.contributors_id.first_name} {contributor.contributors_id.last_name}
-                </Link>
-              </h4>
-              <div dangerouslySetInnerHTML={{ __html: contributor.contributors_id.bio }} />
-            </section>
-          </footer>
-        )
-      })}
+      <footer>
+        <section className="contributors">
+          <h3>{title}</h3>
+          {authors}
+        </section>
+      </footer>
     </cite>
   )
 }
