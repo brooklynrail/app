c624e293162f110c8ab6a302273d4b214bb1c87b
jeremyzilar <jeremyzilar@gmail.com>
2024-06-11 14:47:11 -0400

    filter out article.issues that are null or 0

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -4,44 +4,47 @@ import { Articles, Issues } from "../../lib/types"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const articlePages = await getArticlePages()
-  // NOTE: This is returning articles with no issues.
-  // These are the articles that are part of the "Special Issues"
-  // This might be a BUG, or might be how the REST API is set up.
-  // remove all articles from specialArticlePages that have an empty issues array
-  const articlePagesFiltered = articlePages.filter((article: Articles) => article.issues.length > 0)
-  const articles = articlePagesFiltered.map((article: Articles) => {
-    const year = article.issues[0].issues_id.year
-    const month = article.issues[0].issues_id.month
-    const section = article.sections[0].sections_id.slug
-    const slug = article.slug
+  // // NOTE: This is returning articles with no issues.
+  // // These are the articles that are part of the "Special Issues"
+  // // This might be a BUG, or might be how the REST API is set up.
+  // // remove all articles from specialArticlePages that have an empty issues array
 
-    return {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${year}/${month}/${section}/${slug}/`,
-      lastModified: article.date_updated,
-      changeFrequency: "weekly" as const,
-      priority: 1,
-    }
-  })
+  // const articlePagesFiltered = articlePages.filter((article: Articles) => article.issues && article.issues.length > 0)
+  const articles = articlePages
+    .filter((article: Articles) => article.issues && article.issues.length > 0)
+    .map((article: Articles) => {
+      const year = article.issues[0].issues_id.year
+      const month = article.issues[0].issues_id.month
+      const section = article.sections[0].sections_id.slug
+      const slug = article.slug
+
+      return {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/${year}/${month}/${section}/${slug}/`,
+        lastModified: article.date_updated,
+        changeFrequency: "weekly" as const,
+        priority: 1,
+      }
+    })
 
   const specialArticlePages = await getSpecialArticlePages()
-  // NOTE: This is returning articles with no issues.
-  // These are the articles that are part of the "Special Issues"
-  // This might be a BUG, or might be how the REST API is set up.
-  // remove all articles from specialArticlePages that have an empty issues array
-  const specialArticlePagesFiltered = specialArticlePages.filter((article: Articles) => article.issues.length > 0)
+  // // NOTE: This is returning articles with no issues.
+  // // These are the articles that are part of the "Special Issues"
+  // // This might be a BUG, or might be how the REST API is set up.
+  // // remove all articles from specialArticlePages that have an empty issues array
+  const specialArticles = specialArticlePages
+    .filter((article: Articles) => article.issues && article.issues.length > 0)
+    .map((article: Articles) => {
+      const slug = article.slug
+      const issueSlug = article.issues[0].issues_id.slug
+      const section = article.sections[0].sections_id.slug
 
-  const specialArticles = specialArticlePagesFiltered.map((article: Articles) => {
-    const slug = article.slug
-    const issueSlug = article.issues && article.issues[0].issues_id.slug
-    const section = article.sections && article.sections[0].sections_id.slug
-
-    return {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/special/${issueSlug}/${section}/${slug}/`,
-      lastModified: article.date_updated,
-      changeFrequency: "weekly" as const,
-      priority: 1,
-    }
-  })
+      return {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/special/${issueSlug}/${section}/${slug}/`,
+        lastModified: article.date_updated,
+        changeFrequency: "weekly" as const,
+        priority: 1,
+      }
+    })
 
   const allIssues = await getIssues()
   const issues = allIssues.map((issue: Issues) => {
