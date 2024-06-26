09df4371b48fee43777c71d84aad40b3ad8bb604
jeremyzilar <jeremyzilar@gmail.com>
2024-06-26 12:08:27 -0400

    archive page component

---
+++ src/app/components/archive/index.tsx
@@ -0,0 +1,102 @@
+"use client"
+import Header from "../issuePage/header"
+import { ArticlesIssues, Issues } from "../../../../lib/types"
+import Image from "next/image"
+import { stripHtml } from "string-strip-html"
+import Link from "next/link"
+import { getPermalink, PageType } from "../../../../lib/utils"
+
+export interface PromoProps {
+  currentArticles: ArticlesIssues[]
+  year: number
+  month: number
+}
+
+export interface ArchivePageProps {
+  issues: Issues[]
+  permalink: string
+}
+
+const ArchivePage = (props: ArchivePageProps) => {
+  const { issues } = props
+
+  // list all Issues
+  const issueList = issues.map((issue: Issues) => {
+    const { id, issue_number, special_issue, title, cover_1 } = issue
+    if (!cover_1 || !cover_1.filename_disk) {
+      return null
+    }
+
+    const permalink = getPermalink({
+      year: issue.year,
+      month: issue.month,
+      type: PageType.Issue,
+    })
+
+    const allCovers = [issue.cover_1, issue.cover_2, issue.cover_3, issue.cover_4, issue.cover_5, issue.cover_6]
+    // render all covers if the cover exists
+    const coversList = allCovers.map((cover, index) => {
+      if (!cover || !cover.filename_disk) {
+        return null
+      }
+
+      const alt = cover.caption ? stripHtml(cover.caption).result : `${title} — The Brooklyn Rail`
+
+      return (
+        <li key={index} className="cover">
+          <Image
+            priority
+            id={`cover-${index + 1}`}
+            src={`${process.env.NEXT_PUBLIC_IMAGE_PATH}${cover.filename_disk}`}
+            width={100}
+            height={100}
+            style={{
+              height: "auto",
+            }}
+            alt={alt}
+          />
+        </li>
+      )
+    })
+
+    return (
+      <li key={id} className="issue">
+        <Link href={permalink} title={`Visit ${stripHtml(title).result}`}>
+          <h3>{title}</h3>
+          <ul className="coversList">{coversList}</ul>
+        </Link>
+      </li>
+    )
+  })
+
+  return (
+    <>
+      <div className={`paper`}>
+        <div className="hatbox"></div>
+        <div className="wrapper home">
+          <header role="banner">
+            <div className="grid-container grid-container-desktop">
+              <div className="grid-row">
+                <div className="grid-col-12">
+                  <Header />
+                </div>
+              </div>
+            </div>
+          </header>
+
+          <section id="main">
+            <div className="grid-container grid-container-desktop">
+              <div className="grid-row grid-gap-3">
+                <div className="grid-col-12">
+                  <ul className="issues-archive">{issueList}</ul>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ArchivePage
