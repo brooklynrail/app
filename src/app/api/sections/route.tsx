d6fe2c9d5978bda0b735b3a93cfb77d942aa02a7
jeremyzilar <jeremyzilar@gmail.com>
2024-08-20 11:31:40 -0600

    add getSectionsByIssueId

--- src/app/api/sections/route.tsx
+++ src/app/api/sections/route.tsx
@@ -1,25 +1,13 @@
 import { readItems } from "@directus/sdk"
 import directus from "../../../../lib/directus"
+import { getSectionsByIssueId } from "../../../../lib/utils"
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const issueId = searchParams.get("issueId")
 
   if (issueId) {
-    const data = await directus.request(
-      readItems("sections", {
-        fields: ["id", "name", "slug"],
-        filter: {
-          _and: [
-            {
-              articles: {
-                issue: { id: { _eq: issueId } },
-              },
-            },
-          ],
-        },
-      }),
-    )
+    const data = await getSectionsByIssueId(issueId)
     return Response.json(data)
   } else {
     const data = await directus.request(
