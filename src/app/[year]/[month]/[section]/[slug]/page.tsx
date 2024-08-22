fe782cd308a075c101b58922ec9bc7a10866da77
jeremyzilar <jeremyzilar@gmail.com>
2024-08-21 20:42:52 -0600

    move generateMetadata up the page

--- src/app/[year]/[month]/[section]/[slug]/page.tsx
+++ src/app/[year]/[month]/[section]/[slug]/page.tsx
@@ -8,7 +8,44 @@ import Article from "@/app/components/article"
 // See: https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamicparams
 export const dynamicParams = true
 
-// export const dynamicParams = true
+export async function generateMetadata({ params }: any): Promise<Metadata> {
+  const data = await getData({ params })
+
+  if (!data.props.articleData || !data.props.currentSection || !data.props.permalink) {
+    return {}
+  }
+
+  const { title, excerpt, featured_image, date_created, date_updated, contributors, title_tag, section } =
+    data.props.articleData
+  const ogtitle = title_tag ? stripHtml(title_tag).result : stripHtml(title).result
+  const ogdescription = `${stripHtml(excerpt).result}`
+  const ogimageprops = { ogimage: featured_image, title }
+  const ogimages = getOGImage(ogimageprops)
+
+  const authors = contributors.map((contributor: any) => {
+    const contribPermalink = getPermalink({ type: PageType.Contributor, slug: contributor.contributors_id.slug })
+    return contribPermalink
+  })
+
+  return {
+    title: `${ogtitle} | The Brooklyn Rail`,
+    description: ogdescription,
+    alternates: {
+      canonical: `${data.props.permalink}`,
+    },
+    openGraph: {
+      title: `${ogtitle} | The Brooklyn Rail`,
+      description: ogdescription,
+      url: data.props.permalink,
+      images: ogimages,
+      type: `article`,
+      publishedTime: date_created,
+      modifiedTime: date_updated,
+      section: section.name,
+      authors: authors,
+    },
+  }
+}
 export interface ArticleProps {
   articleData: Articles
   issueBasics: Issues
@@ -17,26 +54,6 @@ export interface ArticleProps {
   errorCode?: number
   errorMessage?: string
 }
-
-// export async function generateStaticParams() {
-//   const articlePages = await getArticlePages()
-
-//   return articlePages.map((article: Articles) => {
-//     // NOTE: This is returning articles with no issues.
-//     // These are the articles that are part of the "Special Issues"
-//     // This might be a BUG, or might be how the REST API is set up.
-//     if (!article.issue) {
-//       return
-//     }
-//     const month = article.issue.month
-//     return {
-//       year: article.issue.year.toString(),
-//       month: month < 10 ? `0${month.toString()}` : month.toString(),
-//       section: article.section.slug,
-//       slug: article.slug,
-//     }
-//   })
-// }
 
 interface ArticleParams {
   year: string
@@ -79,44 +96,6 @@ async function getData({ params }: { params: ArticleParams }) {
       issueBasics,
       currentSection,
       permalink,
-    },
-  }
-}
-
-export async function generateMetadata({ params }: any): Promise<Metadata> {
-  const data = await getData({ params })
-
-  if (!data.props.articleData || !data.props.currentSection || !data.props.permalink) {
-    return {}
-  }
-
-  const { title, excerpt, featured_image, date_created, date_updated, contributors, title_tag, section } =
-    data.props.articleData
-  const ogtitle = title_tag ? stripHtml(title_tag).result : stripHtml(title).result
-  const ogdescription = `${stripHtml(excerpt).result}`
-  const ogimageprops = { ogimage: featured_image, title }
-  const ogimages = getOGImage(ogimageprops)
-
-  const authors = contributors.map((contributor: any) => {
-    const contribPermalink = getPermalink({ type: PageType.Contributor, slug: contributor.contributors_id.slug })
-    return contribPermalink
-  })
-  return {
-    title: `${ogtitle} | The Brooklyn Rail`,
-    description: ogdescription,
-    alternates: {
-      canonical: `${data.props.permalink}`,
-    },
-    openGraph: {
-      title: `${ogtitle} | The Brooklyn Rail`,
-      description: ogdescription,
-      url: data.props.permalink,
-      images: ogimages,
-      type: `article`,
-      publishedTime: date_created,
-      modifiedTime: date_updated,
-      section: section.name,
-      authors: authors,
     },
   }
 }
