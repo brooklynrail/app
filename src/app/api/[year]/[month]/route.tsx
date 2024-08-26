ebf9f77bf26b87f50e513ad5a2fde0b88ca1a429
jeremyzilar <jeremyzilar@gmail.com>
2024-08-26 07:59:00 -0600

    rm logs

--- src/app/api/[year]/[month]/route.tsx
+++ src/app/api/[year]/[month]/route.tsx
@@ -4,10 +4,7 @@ export async function GET(request: Request, { params }: { params: { year: string
   const year = Number(params.year)
   const month = Number(params.month)
 
-  console.log("year:", year)
-  console.log("month:", month)
   const data = await getIssueData({ year: year, month: month })
-  console.log("getIssueData API:", data)
 
   return Response.json(data)
 }
