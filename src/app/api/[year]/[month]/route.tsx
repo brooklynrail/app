b7ccf251e76b87bc8a1c3938a746f35f3b9c876e
jeremyzilar <jeremyzilar@gmail.com>
2024-08-26 07:50:02 -0600

    add debug logging

--- src/app/api/[year]/[month]/route.tsx
+++ src/app/api/[year]/[month]/route.tsx
@@ -4,7 +4,10 @@ export async function GET(request: Request, { params }: { params: { year: string
   const year = Number(params.year)
   const month = Number(params.month)
 
+  console.log("year:", year)
+  console.log("month:", month)
   const data = await getIssueData({ year: year, month: month })
+  console.log("getIssueData API:", data)
 
   return Response.json(data)
 }
