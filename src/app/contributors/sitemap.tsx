6e0f12281bb8682e0ed10194447e8fcd0b0119e3
jeremyzilar <jeremyzilar@gmail.com>
2024-08-26 16:48:08 -0600

    fix contributors error handling

--- src/app/contributors/sitemap.tsx
+++ src/app/contributors/sitemap.tsx
@@ -1,9 +1,13 @@
 import { MetadataRoute } from "next"
 import { getAllContributors } from "../../../lib/utils"
 import { Contributors } from "../../../lib/types"
+import { notFound } from "next/navigation"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   let allContributors = await getAllContributors()
+  if (!allContributors || allContributors.length == 0) {
+    return notFound()
+  }
   // filter out contributors with no articles
   allContributors = allContributors.filter((contributor: Contributors) => contributor.articles.length > 0)
   const contributors = allContributors.map((contributor: Contributors) => {
