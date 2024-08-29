9733508c8f222c8f20f6904b8be19c00c9ad0f8a
jeremyzilar <jeremyzilar@gmail.com>
2024-08-29 14:07:48 -0600

    add refresh API for clearing the cache on individual URLs

---
+++ src/app/api/refresh/route.tsx
@@ -0,0 +1,29 @@
+import { revalidatePath } from "next/cache"
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url)
+    const secret = searchParams.get("secret")
+    const path = searchParams.get("path")
+
+    // Check if the secret matches the expected secret
+    if (secret !== process.env.REVALIDATION_SECRET) {
+      return new Response("Unauthorized", { status: 401 })
+    }
+
+    // Check if the path is provided
+    if (!path) {
+      return new Response("Path is required", { status: 400 })
+    }
+
+    // Start revalidation
+    revalidatePath(path)
+    const message = `Revalidation started for path: ${path}`
+    return new Response(message, { status: 200 })
+  } catch (err) {
+    // Log the error for debugging purposes
+    console.error("Revalidation error:", err)
+    // Return a generic error response
+    return new Response("Error revalidating", { status: 500 })
+  }
+}
