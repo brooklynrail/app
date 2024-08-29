e0c1e0f1e6732c2d96c7b4a57a76599145a620b0
jeremyzilar <jeremyzilar@gmail.com>
2024-08-28 20:35:12 -0600

    add generateStaticParams back to Article pages

--- src/app/[year]/[month]/[section]/[slug]/page.tsx
+++ src/app/[year]/[month]/[section]/[slug]/page.tsx
@@ -1,5 +1,12 @@
 import { stripHtml } from "string-strip-html"
-import { PageType, getArticle, getIssueData, getOGImage, getPermalink } from "../../../../../../lib/utils"
+import {
+  PageType,
+  getArticle,
+  getArticlePages,
+  getIssueData,
+  getOGImage,
+  getPermalink,
+} from "../../../../../../lib/utils"
 import { Articles, Issues, Sections } from "../../../../../../lib/types"
 import { Metadata } from "next"
 import Article from "@/app/components/article"
@@ -119,4 +126,28 @@ export default async function ArticlePageController({ params }: { params: Articl
   }
 
   return <Article {...data.props} />
+}
+
+export async function generateStaticParams() {
+  const articlePages = await getArticlePages()
+
+  if (!articlePages) {
+    return notFound()
+  }
+
+  return articlePages.map((article: Articles) => {
+    // NOTE: This is returning articles with no issues.
+    // These are the articles that are part of the "Special Issues"
+    // This might be a BUG, or might be how the REST API is set up.
+    if (!article.issue) {
+      return
+    }
+    const month = article.issue.month
+    return {
+      year: article.issue.year.toString(),
+      month: month < 10 ? `0${month.toString()}` : month.toString(),
+      section: article.section.slug,
+      slug: article.slug,
+    }
+  })
 }
