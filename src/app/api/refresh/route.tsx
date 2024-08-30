2cb0d03e602acc05716a1ecba986178291804711
jeremyzilar <jeremyzilar@gmail.com>
2024-08-30 09:14:10 -0600

    revert back to GET request

--- src/app/api/refresh/route.tsx
+++ src/app/api/refresh/route.tsx
@@ -1,36 +1,31 @@
-import { NextApiRequest, NextApiResponse } from "next"
 import { revalidatePath } from "next/cache"
 
 export const dynamic = "force-dynamic" // Mark this API as dynamic
 
-// pages/api/refresh.js
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const data = await req.body
-    console.log("Req body", req.body)
-    console.log("Req data", data)
-    const { secret, title, slug, url, articlePath, sectionPath, issuePath } = req.body
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url)
+    const secret = searchParams.get("secret")
+    const path = searchParams.get("path")
 
-    // if (!secret || secret !== process.env.REVALIDATION_SECRET) {
-    //   // Respond with an unauthorized error if the secret is incorrect
-    //   return res.status(401).json({ message: "Unauthorized" })
-    // }
-
-    if (!articlePath) {
-      // Respond with an error if the articlePath is missing
-      return res.status(400).json({ message: "Path is required" })
+    // Check if the secret matches the expected secret
+    if (secret !== process.env.REVALIDATION_SECRET) {
+      return new Response("Unauthorized", { status: 401 })
     }
 
-    try {
-      // Perform revalidation logic
-      revalidatePath(articlePath)
-      return res.json({ message: "Revalidation successful" })
-    } catch (error) {
-      console.error("Error revalidating:", error)
-      return res.status(500).json({ message: "Error revalidating" })
+    // Check if the path is provided
+    if (!path) {
+      return new Response("Path is required", { status: 400 })
     }
-  } else {
-    // If not a POST request, return method not allowed
-    return res.status(405).json({ message: "Method not allowed" })
+
+    // Start revalidation
+    revalidatePath(path)
+    const message = `Revalidation started for path: ${path}`
+    return new Response(message, { status: 200 })
+  } catch (err) {
+    // Log the error for debugging purposes
+    console.error("Revalidation error:", err)
+    // Return a generic error response
+    return new Response("Error revalidating", { status: 500 })
   }
 }
