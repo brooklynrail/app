7a27446596909c5cea1a953abd01be09a93a810f
jeremyzilar <jeremyzilar@gmail.com>
2024-09-03 15:07:10 -0600

    fix the Viewport for the homepage on Mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Issues, Sections } from "../../lib/types"
 import IssuePage from "@/app/components/issuePage"
 import { getAllIssues, getCurrentIssueData, getPermalink, PageType } from "../../lib/utils"
 import { notFound } from "next/navigation"
+import { Viewport } from "next"
 
 // Dynamic segments not included in generateStaticParams are generated on demand.
 // See: https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamicparams
@@ -10,6 +11,12 @@ export const dynamicParams = true
 // Next.js will invalidate the cache when a
 // request comes in, at most once every 60 seconds.
 export const revalidate = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? 600 : 0
+
+// Set the Viewport to show the full page of the Rail on mobile devices
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 0.405,
+}
 
 export enum PageLayout {
   Issue = "issue",
