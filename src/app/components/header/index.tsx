eaeb25584404f01f03b3cedf7777076e5e68df24
jeremyzilar <jeremyzilar@gmail.com>
2024-09-16 12:54:17 -0600

    update Header to include subhead

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 import Banner from "./banner"
 import styles from "./header.module.scss"
+import Button from "../button"
+import ButtonMenu from "../button-menu"
 
 interface HeaderProps {
   special_issue?: boolean | null
@@ -9,22 +11,37 @@ interface HeaderProps {
 }
 
 const Header = (props: HeaderProps) => {
-  const { special_issue, issue_number, title } = props
+  const { title } = props
 
   return (
-    <div id="header_section">
+    <header className={styles.head}>
       <div className="logo">
         <div id={styles.textflag}>
           <h1>The Brooklyn Rail </h1>
           <h2>Critical Perspectives on Art, Politics and Culture</h2>
           {title && <h3>{title}</h3>}
         </div>
-        TKTKHEADER
         <Link href="/">
           <Banner />
         </Link>
+        <div className="px-4">
+          <div className="grid grid-cols-4 tablet:grid-cols-12 gap-4 desktop:gap-6 gap-y-4">
+            <div className="col-span-12">
+              <div className="flex justify-between items-center">
+                <ButtonMenu />
+                <h2 className={styles.subhead}>
+                  Critical Perspectives on Arts, Politics, and Culture <span>·</span> Independent and Free
+                </h2>
+                <div className="flex space-x-4">
+                  <Button text="Subscribe" />
+                  <Button text="Donate" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </header>
   )
 }
 
