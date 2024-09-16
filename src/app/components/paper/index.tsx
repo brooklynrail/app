54d8ab58f1cbf2c9cf4e7cc71ea1771acd9c6419
jeremyzilar <jeremyzilar@gmail.com>
2024-09-15 20:11:51 -0600

    rm unused imports

--- src/app/components/paper/index.tsx
+++ src/app/components/paper/index.tsx
@@ -1,13 +1,4 @@
 "use client"
-import CoversPopup from "../issueRail/coversPopup"
-import Ad970 from "../ads/ad970"
-import { Ads, Articles } from "../../../../lib/types"
-import { useEffect, useState } from "react"
-import { getAds } from "../../../../lib/utils"
-import { PopupProvider } from "../issueRail/popupProvider"
-import PreviewHeader from "../preview/previewHead"
-import Footer from "../footer"
-import { EventPageProps } from "@/app/events/[year]/[month]/[day]/[slug]/page"
 
 export interface PaperProps {
   pageClass: string
