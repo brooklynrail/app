35bd1418baac15da74357fcb2493e528691d15e5
jeremyzilar <jeremyzilar@gmail.com>
2024-09-16 23:37:38 -0600

    add specific styles for Featured Image

--- src/app/components/featuredImage/index.tsx
+++ src/app/components/featuredImage/index.tsx
@@ -2,7 +2,7 @@ import { DirectusFiles } from "../../../../lib/types"
 import Image from "next/image"
 import parse from "html-react-parser"
 import { stripHtml } from "string-strip-html"
-import styles from "../header/header.module.scss"
+import styles from "./featuredImage.module.scss"
 
 interface FeaturedImageProps {
   image: DirectusFiles
@@ -15,7 +15,7 @@ const FeaturedImage = (props: FeaturedImageProps) => {
   const alt = caption ? caption : `${stripHtml(props.title).result}`
 
   return (
-    <div className="featured-image media">
+    <div className={`media ${styles.media}`}>
       <div>
         <Image
           priority
