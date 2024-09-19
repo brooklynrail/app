b5f281af7a947f0033d48909c482abb939b96d69
jeremyzilar <jeremyzilar@gmail.com>
2024-09-19 12:05:18 -0400

    add theme values

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,6 +3,8 @@ import Banner from "./banner"
 import styles from "./header.module.scss"
 import Button from "../button"
 import ButtonMenu from "../button-menu"
+import ThemeToggle from "../themeToggle"
+import { useState } from "react"
 
 interface HeaderProps {
   special_issue?: boolean | null
@@ -12,36 +14,42 @@ interface HeaderProps {
 
 const Header = (props: HeaderProps) => {
   const { title } = props
+  const [theme, setTheme] = useState<string | null>(null)
+
+  const themeSettings = { theme, setTheme }
 
   return (
-    <header className={styles.head}>
-      <div className="logo">
-        <div id={styles.textflag}>
-          <h1>The Brooklyn Rail </h1>
-          <h2>Critical Perspectives on Art, Politics and Culture</h2>
-          {title && <h3>{title}</h3>}
-        </div>
-        <Link href="/">
-          <Banner />
-        </Link>
-        <div className="px-4">
-          <div className="grid grid-cols-4 tablet:grid-cols-12 gap-4 desktop:gap-6 gap-y-4">
-            <div className="col-span-12">
-              <div className="flex justify-between items-center">
-                <ButtonMenu />
-                <h2 className={styles.subhead}>
-                  Critical Perspectives on Arts, Politics, and Culture <span>·</span> Independent and Free
-                </h2>
-                <div className="flex space-x-4">
-                  <Button text="Subscribe" />
-                  <Button text="Donate" />
+    <>
+      <header className={styles.head}>
+        <div className="logo">
+          <div id={styles.textflag}>
+            <h1>The Brooklyn Rail </h1>
+            <h2>Critical Perspectives on Art, Politics and Culture</h2>
+            {title && <h3>{title}</h3>}
+          </div>
+          <Link href="/">
+            <Banner theme={theme} />
+          </Link>
+          <div className="px-4">
+            <div className="grid grid-cols-4 tablet:grid-cols-12 gap-4 desktop:gap-6 gap-y-4">
+              <div className="col-span-12">
+                <div className="flex justify-between items-center">
+                  <ButtonMenu />
+                  <h2 className={styles.subhead}>
+                    Critical Perspectives on Arts, Politics, and Culture <span>·</span> Independent and Free
+                  </h2>
+                  <div className="flex space-x-4">
+                    <Button text="Subscribe" />
+                    <Button text="Donate" />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <ThemeToggle {...themeSettings} />
+    </>
   )
 }
 
