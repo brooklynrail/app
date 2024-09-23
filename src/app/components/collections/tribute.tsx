a47522d851520876f9f9c4476991b53da5a4f3ae
jeremyzilar <jeremyzilar@gmail.com>
2024-09-22 20:25:38 -0400

    Fix Tribute Collection

--- src/app/components/collections/tribute.tsx
+++ src/app/components/collections/tribute.tsx
@@ -1,32 +1,19 @@
 "use client"
 import Link from "next/link"
-import { Articles, Sections, Tributes } from "../../../../lib/types"
+import { Tributes } from "../../../../lib/types"
 import { getPermalink, PageType } from "../../../../lib/utils"
 import CollectionHead from "./head"
 import FeaturedImage from "../featuredImage"
 import { stripHtml } from "string-strip-html"
-import parse from "html-react-parser"
-import Bylines from "./promos/bylines"
-import Kicker from "./promos/kicker"
-import { useEffect, useLayoutEffect, useRef, useState } from "react"
-import Title, { TitleType } from "./promos/title"
 
 const CollectionTribute = (collection: Tributes) => {
   const { title, deck, summary, blurb, excerpt, curators, featured_image } = collection
   // ==================================================
 
-  // get the first article in the section.articles array
-  const leadArticle = collection.articles[0]
-  // get the list of articles in the section.articles array minus the first article
-  const restOfArticles = collection.articles.slice(1, 5)
-
   const permalink = getPermalink({
-    section: collection.slug,
-    type: PageType.Section,
+    tributeSlug: collection.slug,
+    type: PageType.Tribute,
   })
-
-  // const grid_rows = `grid-rows-${restOfArticles.length}`
-  const grid_rows = `grid-rows-1`
 
   return (
     <>
--- src/app/components/homepage/index.tsx
+++ src/app/components/homepage/index.tsx
@@ -6,6 +6,7 @@ import { Articles, Sections, Tributes } from "../../../../lib/types"
 import { PopupProvider } from "../issueRail/popupProvider"
 import CollectionArt from "../collections/art"
 import CollectionArtSeen from "../collections/artSeen"
+import CollectionTribute from "../collections/tribute"
 
 export interface PromoProps {
   currentArticles: Articles[]
@@ -30,22 +31,20 @@ const HomePage = (props: HomePageProps) => {
 
   return (
     <>
-      <PopupProvider>
-        <div className={`paper ${issueClass}`}>
-          <div className="wrapper home">
-            <header role="banner">
-              <div className="grid grid-cols-4 tablet:grid-cols-12 gap-4 desktop:gap-3 gap-y-4">
-                <div className="col-span-12">
-                  <Header />
-                </div>
+      <div className={`paper ${issueClass}`}>
+        <div className="wrapper home">
+          <header role="banner">
+            <div className="grid grid-cols-4 tablet:grid-cols-12 gap-4 desktop:gap-3 gap-y-4">
+              <div className="col-span-12">
+                <Header />
               </div>
-            </header>
+            </div>
+          </header>
 
-            <main>{allCollections}</main>
-          </div>
+          <main>{allCollections}</main>
         </div>
-        <CoversPopup />
-      </PopupProvider>
+      </div>
+      <CoversPopup />
     </>
   )
 }
@@ -54,6 +53,7 @@ export enum Collection {
   Art = "art",
   ArtSeen = "artseen",
   Books = "books",
+  CriticsPage = "criticspage",
   Dance = "dance",
   Film = "film",
   Music = "music",
@@ -68,8 +68,12 @@ const CollectionSection = (collection: Sections) => {
       return <CollectionArt {...collection} />
     case Collection.ArtSeen:
       return <CollectionArtSeen {...collection} />
+      break
     case Collection.Books:
       console.log("Books")
+      break
+    case Collection.CriticsPage:
+      console.log("CriticsPage")
       break
     case Collection.Dance:
       console.log("Dance")
@@ -87,10 +91,6 @@ const CollectionSection = (collection: Sections) => {
       console.log("Default")
       break
   }
-}
-
-const CollectionTribute = (collection: Tributes) => {
-  return <CollectionTribute {...collection} />
 }
 
 export default HomePage
