6083ca82a24141245824899269569438d845dd0c
jeremyzilar <jeremyzilar@gmail.com>
2024-09-23 20:04:11 -0400

    update AD styles

--- src/app/components/ads/ad970.tsx
+++ src/app/components/ads/ad970.tsx
@@ -42,71 +42,63 @@ const Ad970 = (props: Ad970Props) => {
 
   return (
     <>
-      <section className="banner">
-        <div className="grid-container grid-container-desktop">
-          <div className="grid-row">
-            <div className="grid-col-12">
-              <div className="ad ad_970 visible">
-                <p>Advertisement</p>
-                <div>
-                  <Link href={randomAd.ad_url} target="_blank">
-                    <Image
-                      className="image_desktop"
-                      src={srcDesktop}
-                      width={desktopWidth}
-                      height={desktopHeight}
-                      alt={alt}
-                      onLoad={() =>
-                        sendGAEvent("event", "impression", {
-                          event_category: "ads",
-                          event_label: randomAd.slug,
-                          event_value: randomAd.ad_url,
-                          ad_format: "banner",
-                          campaign: randomAd.campaign_title,
-                          campaign_id: randomAd.slug,
-                          ad_source: "br-studio",
-                        })
-                      }
-                      onClick={() =>
-                        sendGAEvent("event", "click", {
-                          event_category: "ads",
-                          event_label: randomAd.slug,
-                          event_value: randomAd.ad_url,
-                          ad_format: "banner",
-                          campaign: randomAd.campaign_title,
-                          campaign_id: randomAd.slug,
-                          ad_source: "br-studio",
-                        })
-                      }
-                    />
-                    <Image
-                      className="image_mobile"
-                      src={srcMobile}
-                      width={mobileWidth}
-                      height={mobileHeight}
-                      alt={alt}
-                      onLoad={() =>
-                        sendGAEvent("event", "impression", {
-                          event_category: "ads",
-                          event_label: randomAd.slug,
-                          event_value: randomAd.ad_url,
-                        })
-                      }
-                      onClick={() =>
-                        sendGAEvent("event", "click", {
-                          event_category: "ads",
-                          event_label: randomAd.slug,
-                          event_value: randomAd.ad_url,
-                        })
-                      }
-                    />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div className="m-0 mx-6">
+        <p className="text-xs text-center uppercase text-gray-500">Advertisement</p>
+        <div>
+          <Link href={randomAd.ad_url} target="_blank">
+            <Image
+              className="hidden tablet:block"
+              src={srcDesktop}
+              width={desktopWidth}
+              height={desktopHeight}
+              alt={alt}
+              onLoad={() =>
+                sendGAEvent("event", "impression", {
+                  event_category: "ads",
+                  event_label: randomAd.slug,
+                  event_value: randomAd.ad_url,
+                  ad_format: "banner",
+                  campaign: randomAd.campaign_title,
+                  campaign_id: randomAd.slug,
+                  ad_source: "br-studio",
+                })
+              }
+              onClick={() =>
+                sendGAEvent("event", "click", {
+                  event_category: "ads",
+                  event_label: randomAd.slug,
+                  event_value: randomAd.ad_url,
+                  ad_format: "banner",
+                  campaign: randomAd.campaign_title,
+                  campaign_id: randomAd.slug,
+                  ad_source: "br-studio",
+                })
+              }
+            />
+            <Image
+              className="block tablet:hidden"
+              src={srcMobile}
+              width={mobileWidth}
+              height={mobileHeight}
+              alt={alt}
+              onLoad={() =>
+                sendGAEvent("event", "impression", {
+                  event_category: "ads",
+                  event_label: randomAd.slug,
+                  event_value: randomAd.ad_url,
+                })
+              }
+              onClick={() =>
+                sendGAEvent("event", "click", {
+                  event_category: "ads",
+                  event_label: randomAd.slug,
+                  event_value: randomAd.ad_url,
+                })
+              }
+            />
+          </Link>
         </div>
-      </section>
+      </div>
     </>
   )
 }
