6842acf48a9a07705738aa56e3f100cb18e8f009
jeremyzilar <jeremyzilar@gmail.com>
2024-09-28 10:54:32 -0400

    button styles

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -1,15 +1,28 @@
-import styles from "../header/header.module.scss"
+import Link from "next/link"
+
+export enum ButtonType {
+  Donate = "bg-red-600 text-white",
+  Subscribe = "bg-white text-black",
+}
 
 interface ButtonProps {
   text: string
+  link: string
+  type: ButtonType
 }
 
 const Button = (props: ButtonProps) => {
-  const { text } = props
+  const { text, link, type } = props
 
   return (
     <>
-      <button className={styles.button}>{text}</button>
+      <Link href={link}>
+        <button
+          className={`${type} font-medium shadow-xl text-xs tablet:text-sm tablet-lg:text-md desktop:text-lg desktop-lg:text-xl px-2.5 py-1.5 tablet:px-3.5 tablet:py-2 desktop:px-6 desktop:py-3 rounded uppercase hover:underline underline-offset-4`}
+        >
+          {text}
+        </button>
+      </Link>
     </>
   )
 }
