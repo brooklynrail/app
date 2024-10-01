6e923e5c0176a0bcd0826bb4488575d77b20bb11
jeremyzilar <jeremyzilar@gmail.com>
2024-10-01 13:59:13 -0400

    add custom styles

--- src/app/components/collections/promos/blurb.tsx
+++ src/app/components/collections/promos/blurb.tsx
@@ -1,4 +1,5 @@
 import parse from "html-react-parser"
+import styles from "../../tributePage/tribute.module.scss"
 
 interface BlurbProps {
   text: string
@@ -8,8 +9,10 @@ const Blurb = (props: BlurbProps) => {
   const { text } = props
 
   // if text, after the opening <p> tag, starts with a quote mark, then -indent the text to hang the quote
-  const hang_quote = text && (text.match(/<p>“/) || text.match(/<p>&ldquo;/)) ? "-indent-2" : ""
+  const hang_quote = text && (text.match(/<p>“/) || text.match(/<p>&ldquo;/)) ? "-indent-3" : ""
 
-  return <div className={`text-2xl tablet:text-3xl font-serif font-normal ${hang_quote}`}>{parse(text)}</div>
+  return (
+    <div className={`text-2xl tablet:text-3xl font-serif font-normal ${styles.blurb} ${hang_quote}`}>{parse(text)}</div>
+  )
 }
 export default Blurb
