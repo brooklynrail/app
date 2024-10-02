113e7ca5f6a0edc416aa4ed0dcc52a5d2edb16ee
jeremyzilar <jeremyzilar@gmail.com>
2024-10-02 16:03:39 -0400

    update header

--- src/app/components/homepage/index.tsx
+++ src/app/components/homepage/index.tsx
@@ -37,7 +37,7 @@ const HomePage = (props: HomePageProps) => {
           <header role="banner">
             <div className="grid grid-cols-4 tablet:grid-cols-12 gap-4 desktop:gap-3 gap-y-4">
               <div className="col-span-12">
-                <Header type={HeaderType.Issue} />
+                <Header type={HeaderType.Default} />
               </div>
             </div>
           </header>
