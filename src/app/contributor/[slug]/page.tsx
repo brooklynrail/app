0bc1922e49ba6ac90c035bbac1ba3e9276b16fea
jeremyzilar <jeremyzilar@gmail.com>
2024-10-03 11:48:13 -0400

    cache contributor pages until revalidated

--- src/app/contributor/[slug]/page.tsx
+++ src/app/contributor/[slug]/page.tsx
@@ -8,10 +8,6 @@ import ContributorPage from "@/app/components/contributor"
 // See: https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamicparams
 export const dynamicParams = true
 
-// Next.js will invalidate the cache when a
-// request comes in, at most once every 60 seconds.
-export const revalidate = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? 600 : 0
-
 export async function generateMetadata({ params }: any): Promise<Metadata> {
   const data = await getData({ params })
 
