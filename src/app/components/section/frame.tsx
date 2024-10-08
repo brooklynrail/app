a7b8a92724363a109109cdeb6f1a13b6d034a423
jeremyzilar <jeremyzilar@gmail.com>
2024-10-08 16:27:41 -0400

    rm border

--- src/app/components/section/frame.tsx
+++ src/app/components/section/frame.tsx
@@ -14,7 +14,7 @@ const Frame = (props: FrameProps) => {
 
   return (
     <div className="frame px-6">
-      <div className="grid grid-cols-4 tablet:grid-cols-12 divide-y rail-divide tablet:divide-y-0 border-t rail-border py-3">
+      <div className="grid grid-cols-4 tablet:grid-cols-12 divide-y rail-divide tablet:divide-y-0 py-3">
         <div
           className={`col-span-4 grid-cols-4 tablet:col-span-6 tablet:col-start-1 tablet:border-r rail-border ${alt ? `alt-odd tablet:pr-3` : `alt-even tablet:pr-6`}`}
         >
