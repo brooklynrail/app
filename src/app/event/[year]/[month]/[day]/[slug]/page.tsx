5f59e4ba8f75631243bf9e96310e108b15927058
jeremyzilar <jeremyzilar@gmail.com>
2024-10-10 14:30:56 -0400

    add events metadata

--- src/app/event/[year]/[month]/[day]/[slug]/page.tsx
+++ src/app/event/[year]/[month]/[day]/[slug]/page.tsx
@@ -1,12 +1,58 @@
 import { notFound } from "next/navigation"
-import { Events } from "../../../../../../../lib/types"
+import { Events, EventsPeople, People } from "../../../../../../../lib/types"
 import { getPermalink, PageType } from "../../../../../../../lib/utils"
-import { getEvent, getEvents } from "../../../../../../../lib/utils/events/utils"
+import { getEvent } from "../../../../../../../lib/utils/events/utils"
 import EventPage from "@/app/components/event"
+import { Metadata } from "next"
+import { stripHtml } from "string-strip-html"
 
 // Dynamic segments not included in generateStaticParams are generated on demand.
 // See: https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamicparams
 export const dynamicParams = true
+
+export async function generateMetadata({ params }: any): Promise<Metadata> {
+  const data = await getData({ params })
+
+  if (!data.eventData || !data.permalink) {
+    return {}
+  }
+
+  const { title, excerpt, date_created, date_updated, people, title_tag, section } = data.eventData
+  const ogtitle = title_tag ? stripHtml(title_tag).result : stripHtml(title).result
+  const ogdescription = `${stripHtml(excerpt).result}`
+  // const ogimageprops = { ogimage: featured_image, title }
+  // const ogimages = getOGImage(ogimageprops)
+
+  const authors = people.map((person: EventsPeople) => {
+    if (!person || !person.people_id) {
+      return ""
+    }
+    const personPermalink = getPermalink({ type: PageType.Person, personSlug: person.people_id.slug })
+    return personPermalink
+  })
+
+  return {
+    title: `${ogtitle}`,
+    description: ogdescription,
+    alternates: {
+      canonical: `${data.permalink}`,
+    },
+    openGraph: {
+      title: `${ogtitle}`,
+      description: ogdescription,
+      url: data.permalink,
+      // images: ogimages,
+      type: `article`,
+      publishedTime: date_created,
+      modifiedTime: date_updated,
+      section: section.name,
+      authors: authors,
+    },
+    twitter: {
+      // images: ogimages,
+    },
+  }
+}
 
 export interface EventProps {
   eventData: Events
