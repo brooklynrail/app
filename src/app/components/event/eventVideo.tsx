12705941c6f7bc3d9affc237ef71517b0bcdc296
jeremyzilar <jeremyzilar@gmail.com>
2024-10-10 21:26:02 -0400

    update youtube embed

--- src/app/components/event/eventVideo.tsx
+++ src/app/components/event/eventVideo.tsx
@@ -6,12 +6,14 @@ interface EventVideoProps {
 }
 const EventVideo = (props: EventVideoProps) => {
   const { youtube_id, title } = props
+  console.log("youtube_id", youtube_id)
+  const youtube_url = `https://www.youtube.com/embed/${youtube_id}?autoplay=1`
   return (
     <div className="col-span-4 desktop:col-span-10 desktop:col-start-2">
       <div className="relative w-full pb-[56.25%]">
         <iframe
           className="absolute top-0 left-0 w-full h-full"
-          src={`https://www.youtube.com/embed/scHun3qBUV0?si=${youtube_id}`}
+          src={youtube_url}
           title={title}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
