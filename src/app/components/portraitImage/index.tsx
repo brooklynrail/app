13093d162413dccc2833072dcf9b8e0939c88228
jeremyzilar <jeremyzilar@gmail.com>
2024-10-16 11:28:28 -0400

    Update the ALT text for portrait images

--- src/app/components/portraitImage/index.tsx
+++ src/app/components/portraitImage/index.tsx
@@ -10,7 +10,9 @@ interface PortraitImageProps {
 const PortraitImage = (props: PortraitImageProps) => {
   const { filename_disk, caption, width, height, alt } = props.image
   const src = `${process.env.NEXT_PUBLIC_IMAGE_PATH}${filename_disk}?key=portrait`
-  const alt_text = alt ? alt : `A photo of ${stripHtml(props.title).result}`
+  const alt_text = alt
+    ? alt
+    : `A photo of ${stripHtml(props.title).result} on The Brooklyn Rail's New Social Environment`
 
   return (
     <div
