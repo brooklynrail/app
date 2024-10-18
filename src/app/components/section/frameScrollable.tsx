10c3fa4238d8117326b62191db72c1ec932b1717
jeremyzilar <jeremyzilar@gmail.com>
2024-10-18 14:27:46 -0400

    rm bottom border

--- src/app/components/section/frameScrollable.tsx
+++ src/app/components/section/frameScrollable.tsx
@@ -15,7 +15,7 @@ const FrameScrollable = (props: FrameScrollableProps) => {
     <div>
       <div className="grid grid-cols-4 tablet-lg:grid-cols-12 gap-3">
         <div className="col-span-4 tablet-lg:col-span-12">
-          <div className="tablet:px-6 pb-3 border-b rail-border">
+          <div className="tablet:px-6">
             <div className="pl-3 tablet:pl-0 divide-x rail-divide flex overflow-x-auto snap-mandatory snap-x scroll-smooth">
               {Promos}
             </div>
