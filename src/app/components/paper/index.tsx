da93a3b5e84d58cb7aa3bc837c5a8fb6c48e5a01
jeremyzilar <jeremyzilar@gmail.com>
2024-10-20 14:16:31 -0400

    add preview banner

--- src/app/components/paper/index.tsx
+++ src/app/components/paper/index.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from "next/link"
 import Ad970 from "../ads/ad970"
 import Footer from "../footer"
 import CoversPopup from "../issueRail/coversPopup"
@@ -22,6 +23,7 @@ const Paper = (props: PaperProps) => {
   return (
     <PopupProvider hidePopup={hidePopup}>
       <div className={`paper ${pageClass}`}>
+        <PreviewBar />
         {children}
         <Footer />
         <Ad970 />
@@ -29,6 +31,26 @@ const Paper = (props: PaperProps) => {
         <PopupDonate />
       </div>
     </PopupProvider>
+  )
+}
+
+const PreviewBar = () => {
+  const copy = "Welcome to our PREVIEW site. "
+
+  return (
+    <div className="bg-amber-400">
+      <p className="text-center py-1">
+        ★ {copy}
+        <Link
+          href="https://docs.google.com/document/d/1LNd2CRu8yKE68PGxdI8sfrNjuGXuDrAno9Y0GB4i0Y4/edit"
+          target="_new"
+          className="underline "
+        >
+          Please leave us feedback
+        </Link>{" "}
+        »
+      </p>
+    </div>
   )
 }
 
