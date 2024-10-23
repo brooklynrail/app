0b2c46982c4d47fbe121d4502b1630ccf1ab7360
jeremyzilar <jeremyzilar@gmail.com>
2024-10-22 21:17:51 -0400

    new article header

--- src/app/components/header/default.tsx
+++ src/app/components/header/default.tsx
@@ -15,32 +15,17 @@ const HeaderDefault = (props: HeaderProps) => {
   return (
     <>
       <header id={styles.rail_header} className={`border-b rail-border rail-header-${type}`}>
-        <div>
-          <div className="hidden">
-            <h1>The Brooklyn Rail </h1>
-            <h2>Critical Perspectives on Art, Politics and Culture</h2>
-            {title && <h3>{title}</h3>}
-          </div>
-          <div className="px-3 py-3 tablet-lg:px-6">
-            <div className="flex w-full items-center space-x-4">
-              <Link href={permalink} className="w-full">
-                <Banner type={type} />
-              </Link>
-              <div className="tablet:hidden">
-                <MenuButton classes={`w-[7vw] h-[7vw]`} collections={navData.collections} />
-              </div>
-            </div>
-          </div>
-          <div className="hidden tablet:block px-3 tablet-lg:px-6 pb-3">
-            <div className="flex space-x-3 justify-between">
-              <div className="flex flex-col items-end tablet:flex-row tablet:items-center justify-center w-full space-y-3 tablet:space-y-0 tablet:space-x-6 desktop:space-x-6">
-                <Subhead />
-                <div className="flex items-center space-x-3 desktop:space-x-6">
-                  <SubscribeButton />
-                  <DonateButton />
-                </div>
-              </div>
-            </div>
+        <div className="hidden">
+          <h1>The Brooklyn Rail </h1>
+          <h2>Critical Perspectives on Art, Politics and Culture</h2>
+          {title && <h3>{title}</h3>}
+        </div>
+        <div className="relative">
+          <div className="p-3 tablet:px-6">
+            <Link href={permalink} className="w-full space-y-3">
+              <Banner type={type} />
+              <Subhead />
+            </Link>
           </div>
         </div>
       </header>
