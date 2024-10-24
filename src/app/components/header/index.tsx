5bf764512c2d49c848a0bf09b1e65ac696ab44eb
jeremyzilar <jeremyzilar@gmail.com>
2024-10-24 12:42:19 -0400

    -20rem

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -96,7 +96,7 @@ const HeaderHomepage = (props: HeaderProps) => {
         {title && <h3>{title}</h3>}
       </div>
 
-      <div className="relative h-[calc(100vh-16rem)] tablet-lg:h-[calc(100vh-11.5rem)]">
+      <div className="relative h-[calc(100vh-20rem)] tablet-lg:h-[calc(100vh-11.5rem)]">
         <VideoBG videoRef={videoRef} />
         <div className="sticky top-0">
           <div className="p-3 pb-9 tablet:px-6">
