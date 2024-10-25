c92128f1ba2339eff703609704bb392e74fffc75
jeremyzilar <jeremyzilar@gmail.com>
2024-10-24 20:43:51 -0400

    fix padding on collection head

--- src/app/components/collections/head.tsx
+++ src/app/components/collections/head.tsx
@@ -22,7 +22,7 @@ const CollectionHead = (props: CollectionHeadProps) => {
   )
   return (
     <div
-      className={`head px-3 tablet-lg:px-6 pt-3 pb-1.5 tablet-lg:pt-6 tablet-lg:pb-3 sticky top-9 ${classes ? classes : "rail-bg"} z-10`}
+      className={`head px-3 tablet-lg:px-6 pt-3 pb-1.5 tablet-lg:pt-6 tablet-lg:pb-3 sticky top-11 tablet-lg:top-12 ${classes ? classes : "rail-bg"} z-10`}
     >
       <div className="grid grid-cols-4 tablet-lg:grid-cols-12 gap-3">
         <div className="col-span-4 tablet-lg:col-span-12">
