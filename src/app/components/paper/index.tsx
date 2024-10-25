2fd216f344f392b37739a54522d4e3f51735bd89
jeremyzilar <jeremyzilar@gmail.com>
2024-10-24 22:56:46 -0400

    password protect the preview site

--- src/app/components/paper/index.tsx
+++ src/app/components/paper/index.tsx
@@ -9,6 +9,8 @@ import Header from "../header"
 import { Homepage, HomepageBanners, Issues } from "../../../../lib/types"
 import NavBar from "../navBar"
 import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
+import Password from "../preview/password"
 
 export interface PaperProps {
   pageClass: string
@@ -35,6 +37,50 @@ const Paper = (props: PaperProps) => {
   const { theme, setTheme } = useTheme()
   const pathname = usePathname()
   const isHomepage = pathname === "/"
+
+  // PASSWORD
+  // cookieSlug is the cookie that gets set after you enter the password
+  const cookieSlug = `rail_preview_site`
+  const [password, setPassword] = useState("")
+  const [isViewable, setIsViewable] = useState(false)
+  const [isStudioPreview, setIsStudioPreview] = useState(false)
+  const [passwordError, setPasswordError] = useState<string | undefined>()
+
+  // the isStudioPreview param is set to `true` when you are logged in to Directus
+  // if either of these cookies are set, the article will be viewable
+  useEffect(() => {
+    // get the URL of the current page
+    const currentUrl = window.location.href
+
+    // If the Preview Password has been previously entered, the preview cookie should be set
+    if (isViewable) {
+      setIsViewable(true)
+    }
+  }, [isStudioPreview, isViewable])
+
+  const handlePasswordSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+
+    // Check if the `password` is the same as `previewPassword`
+    if (password === `juniper`) {
+      // Redirect to the article preview
+      setIsViewable(true)
+      // set a cookie
+      document.cookie = `${cookieSlug}=true; path=/; max-age=3600; SameSite=None; Secure`
+    } else {
+      // Show an error message for incorrect password
+      setPasswordError("Incorrect password")
+    }
+  }
+
+  const passwordProps = { password, passwordError, setPassword, handlePasswordSubmit }
+
+  const isPreviewSite = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+  console.log("isPreviewSite", isPreviewSite)
+
+  if (!isViewable || isPreviewSite) {
+    return <Password {...passwordProps} />
+  }
 
   return (
     <PopupProvider hidePopup={hidePopup}>
