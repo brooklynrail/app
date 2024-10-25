38e41feb38c556c7f02e08bb2afbd98affb1703e
jeremyzilar <jeremyzilar@gmail.com>
2024-10-24 22:33:06 -0400

    dark mode color updates

--- src/app/components/events/past.tsx
+++ src/app/components/events/past.tsx
@@ -4,13 +4,11 @@ import Link from "next/link"
 import Paper, { PaperType } from "../paper"
 import PastEventsList from "./pastEventsList"
 
-const limit = 16 * 2
-
 const PastEventsPage = (props: PastEventsProps) => {
   const { navData } = props
   return (
-    <Paper pageClass="paper-events-past" type={PaperType.Default} navData={navData}>
-      <main className="px-6 tablet-lg:px-3 pb-12 desktop:max-w-screen-widescreen mx-auto">
+    <Paper pageClass="paper-events-past" type={PaperType.Events} navData={navData}>
+      <main className="px-3 tablet-lg:px-6 pb-12 desktop:max-w-screen-widescreen mx-auto">
         <div className="space-y-9 divide-y rail-divide">
           <div className="pt-9 space-y-3 tablet-lg:space-y-6">
             <h1 className="font-bold text-4xl tablet-lg:text-5xl">Past Events</h1>
