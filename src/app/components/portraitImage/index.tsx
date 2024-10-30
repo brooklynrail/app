695d461c60192fc089e441e20b32b75f6693afd7
jeremyzilar <jeremyzilar@gmail.com>
2024-10-30 16:22:01 -0400

    update image transformation settings for People

--- src/app/components/portraitImage/index.tsx
+++ src/app/components/portraitImage/index.tsx
@@ -9,7 +9,7 @@ interface PortraitImageProps {
 }
 const PortraitImage = (props: PortraitImageProps) => {
   const { filename_disk, caption, width, height, alt } = props.image
-  const src = `${process.env.NEXT_PUBLIC_IMAGE_PATH}${filename_disk}?key=portrait`
+  const src = `${process.env.NEXT_PUBLIC_IMAGE_PATH}${filename_disk}?fit=cover&width=300&height=300&quality=85`
   const alt_text = alt
     ? alt
     : `A photo of ${stripHtml(props.title).result} on The Brooklyn Rail's New Social Environment`
@@ -20,7 +20,7 @@ const PortraitImage = (props: PortraitImageProps) => {
       className="float-right w-28 tablet:w-32 desktop:w-40 m-3 mr-0 tablet-lg:m-0 tablet-lg:float-none tablet-lg:flex-none space-y-1"
     >
       <Image
-        className="rounded-full w-28 h-28 tablet:w-32 tablet:h-32 desktop:w-40 desktop:h-40"
+        className="object-cover rounded-full"
         src={src}
         sizes="25vw"
         width={width}
