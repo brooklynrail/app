0827b73e1becba13898711af3249165980b79afe
jeremyzilar <jeremyzilar@gmail.com>
2024-11-12 20:02:56 -0500

    add a basic tool to bust the cache on any page on the site

---
+++ src/app/api/refresh/path/route.tsx
@@ -0,0 +1,30 @@
+import { revalidatePath } from "next/cache"
+import Link from "next/link"
+export const dynamic = "force-dynamic" // Mark this API as dynamic
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url)
+    const secret = searchParams.get("secret")
+    const path = searchParams.get("path")
+
+    // Check if the secret matches the expected secret
+    if (secret !== process.env.REVALIDATION_SECRET) {
+      return new Response("Unauthorized", { status: 401 })
+    }
+
+    // Check if the path is provided
+    if (!path) {
+      return new Response("path is required", { status: 400 })
+    }
+
+    revalidatePath(path)
+
+    return new Response(`${process.env.NEXT_PUBLIC_BASE_URL}${path}`, { status: 200 })
+  } catch (err) {
+    // Log the error for debugging purposes
+    console.error("Revalidation error:", err)
+    // Return a generic error response
+    return new Response("Error revalidating", { status: 500 })
+  }
+}
