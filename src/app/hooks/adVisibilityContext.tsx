a3e4604709d2de93c1c63609bd6848eccd4bd51e
jeremyzilar <jeremyzilar@gmail.com>
2024-11-14 21:24:39 -0500

    logic for keeping Ad closed per-session

--- src/app/hooks/adVisibilityContext.tsx
+++ src/app/hooks/adVisibilityContext.tsx
@@ -1,3 +1,4 @@
+import { usePathname, useRouter } from "next/navigation"
 import { createContext, useContext, useState, useCallback, useEffect, ReactNode } from "react"
 
 interface AdVisibilityContextProps {
@@ -8,17 +9,33 @@ interface AdVisibilityContextProps {
 const AdVisibilityContext = createContext<AdVisibilityContextProps | undefined>(undefined)
 
 export const AdVisibilityProvider = ({ children }: { children: ReactNode }) => {
-  const [isAdVisible, setIsAdVisible] = useState(true)
-
-  useEffect(() => {
-    // Reset visibility to true on each page load
-    setIsAdVisible(true)
-  }, [])
+  const pathname = usePathname()
+  // Initialize from sessionStorage once per session
+  const [isAdVisible, setIsAdVisible] = useState<boolean>(() => {
+    const storedValue = sessionStorage.getItem("isAdVisible")
+    return storedValue !== null ? JSON.parse(storedValue) : true
+  })
 
   const closeAd = useCallback(() => {
     setIsAdVisible(false)
+    sessionStorage.setItem("isAdVisible", JSON.stringify(false))
   }, [])
 
+  useEffect(() => {
+    // Check if the current path is '/' or starts with '/section/'
+    const shouldShowAd =
+      pathname === "/" ||
+      pathname.startsWith("/section/") ||
+      pathname.startsWith("/events/") ||
+      pathname.startsWith("/issue/")
+
+    if (shouldShowAd) {
+      setIsAdVisible(true)
+      sessionStorage.setItem("isAdVisible", JSON.stringify(true))
+    }
+  }, [pathname])
+
+  // No need for an additional useEffect because session storage persists the value for the current session
   return <AdVisibilityContext.Provider value={{ isAdVisible, closeAd }}>{children}</AdVisibilityContext.Provider>
 }
 
