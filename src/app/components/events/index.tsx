3c25da1889710c3b5c5cd4871e0a545090fccfa2
jeremyzilar <jeremyzilar@gmail.com>
2024-12-03 13:40:05 -0500

    add NSE break copy

--- src/app/components/events/index.tsx
+++ src/app/components/events/index.tsx
@@ -29,6 +29,29 @@ const EventsPage = (props: EventsProps) => {
           <div className="divide-y rail-divide">
             {allEvents}
             <div className="py-12">
+              <div className="max-w-screen-tablet mx-auto space-y-3">
+                <div className="text-center text-lg">
+                  <p>❄️</p>
+                  <h2>The New Social Environment is on break</h2>
+                </div>
+                <p className="text-center">
+                  We look forward to seeing you <strong>Monday, January 6, 2025</strong> for more live dialogues with
+                  artists, filmmakers, writers, and poets. In the meantime, dive into our archive.
+                </p>
+                <p className="text-center">
+                  <a href="https://brooklynrail.org/newsletter" title="Sign up for the newsletter">
+                    <button
+                      className="bg-zinc-800 dark:bg-slate-100 text-slate-100 dark:text-zinc-800 px-3 py-1 rounded-md"
+                      type="button"
+                      name="register"
+                    >
+                      <span>Get notified</span>
+                    </button>
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="py-12">
               <div className="max-w-screen-tablet-lg mx-auto">
                 <Sponsor />
               </div>
