f61bb1d12ee7b745d0c4e6b1a6261c9d58251ead
jeremyzilar <jeremyzilar@gmail.com>
2024-12-04 11:09:00 -0500

    add new video

--- src/app/components/header/videobg.tsx
+++ src/app/components/header/videobg.tsx
@@ -14,7 +14,7 @@ const VideoBG = (props: VideoBGProps) => {
       className="absolute top-0 left-0 w-full h-full object-cover transform"
       poster="/video/loren-munk.png"
     >
-      <source src="https://studio.brooklynrail.org/assets/b1a9a7a8-dda0-44c8-a683-c66a7ead83bb.mp4" type="video/mp4" />
+      <source src="https://studio.brooklynrail.org/assets/3c4a0e44-6512-4997-84dd-76c1d9f35b0d.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   )
