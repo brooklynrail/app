def59208b2c1cc3441c9c3858432a83b6f2dad99
jeremyzilar <jeremyzilar@gmail.com>
2024-12-09 23:13:47 -0500

    add show_featured

--- lib/types.ts
+++ lib/types.ts
@@ -100,6 +100,7 @@ export type Collections = {
   kicker?: string | null
   description?: string | null
   links?: CollectionLinks[] | null
+  show_featured: boolean
   tribute?: Tributes | null
   type: string
   banner_type?: string | null
