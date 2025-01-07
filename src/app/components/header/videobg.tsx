48a7e05ea918773ca62612efab541045d57ad192
jeremyzilar <jeremyzilar@gmail.com>
2025-01-07 10:31:01 -0500

    position the new video

--- src/app/components/header/videobg.tsx
+++ src/app/components/header/videobg.tsx
@@ -12,6 +12,9 @@ const VideoBG = (props: VideoBGProps) => {
       loop
       playsInline
       className="absolute top-0 left-0 w-full h-full object-cover transform"
+      style={{
+        objectPosition: "center 10%",
+      }}
       poster="/video/shirin-neshat-2.png"
     >
       <source src="https://studio.brooklynrail.org/assets/c2da0ae6-d635-44c2-a323-0a090a6f9569.mp4" type="video/mp4" />
