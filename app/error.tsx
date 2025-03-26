452b5fee2585e34acec45c1b7523530922159e21
jeremyzilar <jeremyzilar@gmail.com>
2025-03-25 22:51:17 -0400

    update error page

--- app/error.tsx
+++ app/error.tsx
@@ -13,8 +13,8 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
   }, [error])
 
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-xl font-bold mb-4">Something went wrong loading this page!</h2>
+    <div className="p-6 flex flex-col items-center justify-center w-screen h-screen bg-white dark:bg-zinc-700">
+      <h2 className="text-xl text-center font-bold mb-4">Something went wrong loading this page!</h2>
       <button className="px-4 py-2 bg-red-500 text-white rounded" onClick={() => reset()}>
         Try again
       </button>
