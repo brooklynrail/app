f9065384bd70cba78f3ccf87b4001b50951c2529
jeremyzilar <jeremyzilar@gmail.com>
2025-09-02 08:45:36 -0600

    Update sponsor to Teiger Foundation

--- components/events/sponsor.tsx
+++ components/events/sponsor.tsx
@@ -7,19 +7,19 @@ const Sponsor = () => {
     <div className="px-3 py-6 tablet-lg:p-6 space-y-3 text-md tablet-lg:text-lg text-center p-description bg-white dark:bg-zinc-700 rounded-xl font-medium text-zinc-600 dark:text-indigo-50">
       <p className="text-center">
         ❤️ 🌈 We'd like to thank the{" "}
-        <Link className="text-indigo-500 dark:text-indigo-400" href="https://www.instagram.com/terraamericanart/">
-          The Terra Foundation for American Art
+        <Link className="text-indigo-500 dark:text-indigo-400" href="https://teigerfoundation.org/">
+          The Teiger Foundation
         </Link>{" "}
         for making these daily conversations possible, and for their support of our growing archive.
       </p>
       <p className="text-center divide-x rail-divide">
         <Link
           className="text-sm px-3 text-indigo-500 dark:text-indigo-400"
-          href="https://www.instagram.com/terraamericanart/"
+          href="https://www.instagram.com/teigerfoundation/"
         >
-          <i className="fab fa-instagram"></i> <span>Follow @terraamericanart</span>
+          <i className="fab fa-instagram"></i> <span>Follow @teigerfoundation</span>
         </Link>
-        <Link className="text-sm px-3 text-indigo-500 dark:text-indigo-400" href="https://www.terraamericanart.org/">
+        <Link className="text-sm px-3 text-indigo-500 dark:text-indigo-400" href="https://teigerfoundation.org/">
           <span>Learn more »</span>
         </Link>
       </p>
