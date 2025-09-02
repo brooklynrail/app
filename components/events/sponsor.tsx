63da02e16d1b72dec34e980c425d06eef9f22331
jeremyzilar <jeremyzilar@gmail.com>
2025-09-02 09:07:18 -0600

    update sponsor copy

--- components/events/sponsor.tsx
+++ components/events/sponsor.tsx
@@ -6,13 +6,17 @@ const Sponsor = () => {
   return (
     <div className="px-3 py-6 tablet-lg:p-6 space-y-3 text-md tablet-lg:text-lg text-center p-description bg-white dark:bg-zinc-700 rounded-xl font-medium text-zinc-600 dark:text-indigo-50">
       <p className="text-center">
-        ❤️ 🌈 We'd like to thank the{" "}
+        ❤️ 🌈 We'd like to thank{" "}
+        <Link className="text-indigo-500 dark:text-indigo-400" href="https://www.instagram.com/terraamericanart/">
+          the Terra Foundation for American Art
+        </Link>{" "}
+        and{" "}
         <Link className="text-indigo-500 dark:text-indigo-400" href="https://teigerfoundation.org/">
-          The Teiger Foundation
+          Teiger Foundation
         </Link>{" "}
         for making these daily conversations possible, and for their support of our growing archive.
       </p>
-      <p className="text-center divide-x rail-divide">
+      <p className="text-center divide-x rail-divide hidden">
         <Link
           className="text-sm px-3 text-indigo-500 dark:text-indigo-400"
           href="https://www.instagram.com/teigerfoundation/"
